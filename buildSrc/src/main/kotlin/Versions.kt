b40b0959a9053867857941886fe79ab6432d3478
Christian Kollmann <christian.kollmann@a-sit.at>
2023-02-16 15:10:09 +0100

    CI: Add GitHub workflow definition

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -7,7 +7,7 @@ object Versions {
     const val ktor = "2.2.1"
     const val napier = "2.6.1"
     const val uuid = "0.5.0"
-    const val resultlib = "1.5.0-SNAPSHOT"
+    const val resultlib = "1.4.0"
     const val encoding = "1.1.3"
     const val okio = "3.2.0"
 
