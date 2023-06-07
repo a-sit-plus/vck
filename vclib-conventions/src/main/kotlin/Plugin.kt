2f3e94b3cb210efb95d0ca531cce0cbe62ba10d3
Bernd Prünster <bernd.pruenster@a-sit.at>
2023-06-07 20:03:40 +0200

    encapsulate dependencies potentially useful for other depending projects

--- vclib-conventions/src/main/kotlin/Plugin.kt
+++ vclib-conventions/src/main/kotlin/Plugin.kt
@@ -7,14 +7,21 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.jetbrains.kotlin.gradle.plugin.KotlinDependencyHandler
 
+
+inline fun commonDependencies() = listOf(
+    coroutines(),
+    serialization("json"),
+    napier(),
+    "com.benasher44:uuid:${VcLibVersions.uuid}",
+    "com.squareup.okio:okio:${VcLibVersions.okio}"
+
+
+)
+
 inline fun KotlinDependencyHandler.commonImplementationDependencies() {
-    implementation(serialization("json"))
-    implementation(coroutines())
-    implementation("com.benasher44:uuid:${VcLibVersions.uuid}")
+    commonDependencies().forEach { dep -> implementation(dep) }
     implementation(ktor("http"))
     implementation(ktor("utils"))
-    implementation("com.squareup.okio:okio:${VcLibVersions.okio}")
-    implementation(napier())
 }
 
 fun commonIosExports() = arrayOf(
@@ -27,7 +34,7 @@ fun commonIosExports() = arrayOf(
 
 class VcLibConventions : Plugin<Project> {
     override fun apply(target: Project) {
-       target.plugins.apply("at.asitplus.gradle.conventions")
+        target.plugins.apply("at.asitplus.gradle.conventions")
     }
 }
 
