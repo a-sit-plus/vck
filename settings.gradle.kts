1f612946f6e1c8d8d2b2642d96e0e329c6a1f153
Bernd Prünster <bernd.pruenster@a-sit.at>
2023-08-03 10:31:23 +0200

    experiment with serialization as composite build

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,3 +6,12 @@ rootProject.name = "vclibrary"
 include(":vclib")
 include(":vclib-aries")
 include(":vclib-openid")
+
+includeBuild("kotlinx.serialization"){
+    dependencySubstitution {
+        substitute(module("org.jetbrains.kotlinx:kotlinx-serialization-core")).using(project(":kotlinx-serialization-core"))
+        substitute(module("org.jetbrains.kotlinx:kotlinx-serialization-cbor")).using(project(":kotlinx-serialization-cbor"))
+        substitute(module("org.jetbrains.kotlinx:kotlinx-serialization-json")).using(project(":kotlinx-serialization-json"))
+        substitute(module("org.jetbrains.kotlinx:kotlinx-serialization-properties")).using(project(":kotlinx-serialization-properties"))
+    }
+}
