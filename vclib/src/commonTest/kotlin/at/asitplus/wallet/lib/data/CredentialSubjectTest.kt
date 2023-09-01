48474905ced13dc02e42f74d5d9ef7ec8ba35f5d
Jakob Heher <jakob.heher@iaik.tugraz.at>
2023-09-01 12:02:19 +0200

    Make CredentialSubject abstract (#15)

---
+++ vclib/src/commonTest/kotlin/at/asitplus/wallet/lib/data/CredentialSubjectTest.kt
@@ -0,0 +1,18 @@
+package at.asitplus.wallet.lib.data
+
+import io.kotest.core.spec.style.FreeSpec
+import io.kotest.matchers.shouldBe
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+
+class CredentialSubjectTest : FreeSpec({
+    "Subclasses are correctly deserialized" {
+        @Serializable
+        class SpecializedCredentialTest(override val id: String, @SerialName("not-foo") val foo: String): CredentialSubject()
+        val result = Json.decodeFromString<SpecializedCredentialTest>("{\"id\":\"Test\",\"not-foo\":\"bar\"}")
+        result.id shouldBe "Test"
+        result.foo shouldBe "bar"
+    }
+})
