175ee1fb646a414b284fc2af4136b509cd29541f
Bernd Prünster <bernd.pruenster@a-sit.at>
2023-09-27 13:04:04 +0200

    minor fixes

--- vclib/src/commonTest/kotlin/at/asitplus/wallet/lib/data/CredentialSubjectTest.kt
+++ vclib/src/commonTest/kotlin/at/asitplus/wallet/lib/data/CredentialSubjectTest.kt
@@ -4,15 +4,13 @@ import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.encodeToString
 import kotlinx.serialization.json.Json
 
 class CredentialSubjectTest : FreeSpec({
     "Subclasses are correctly deserialized" {
         @Serializable
-        class SpecializedCredentialTest(override val id: String, @SerialName("not-foo") val foo: String): CredentialSubject(
-            id
-        )
+        class SpecializedCredentialTest(override val id: String, @SerialName("not-foo") val foo: String) :
+            CredentialSubject()
         val result = Json.decodeFromString<SpecializedCredentialTest>("{\"id\":\"Test\",\"not-foo\":\"bar\"}")
         result.id shouldBe "Test"
         result.foo shouldBe "bar"
