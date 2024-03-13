ad2f89688cad2847592c2794b34baba6ad8bab56
acrusage <139230478+acrusage@users.noreply.github.com>
2024-03-13 17:49:37 +0100

    depend on released KMP-Crypto

--- conventions-vclib/src/main/kotlin/VcLibVersions.kt
+++ conventions-vclib/src/main/kotlin/VcLibVersions.kt
@@ -3,7 +3,7 @@ object VcLibVersions {
     const val resultlib = "1.5.3"
     const val encoding = "1.2.3"
     const val okio = "3.5.0"
-    const val kmpcrypto = "2.5.0-SNAPSHOT"
+    const val kmpcrypto = "2.5.0"
     const val bignum = "0.3.9"
 
     object Jvm {
