d5d4013cb08e2d3632cc47a8d7aaaea170755636
acrusage <139230478+acrusage@users.noreply.github.com>
2024-04-08 23:02:15 +0200

    Add: Add presentationDefinitionUri as possible AuthenticationRequestParameter

--- vclib-openid/src/commonMain/kotlin/at/asitplus/wallet/lib/oidc/AuthenticationRequestParameters.kt
+++ vclib-openid/src/commonMain/kotlin/at/asitplus/wallet/lib/oidc/AuthenticationRequestParameters.kt
@@ -140,6 +140,14 @@ data class AuthenticationRequestParameters(
     val presentationDefinition: PresentationDefinition? = null,
 
     /**
+     * OID4VP: A string containing a Presentation Definition JSON object. This parameter MUST be present when
+     * `presentation_definition_uri` parameter, or a `scope` value representing a Presentation Definition is not
+     * present.
+     */
+    @SerialName(AuthenticationRequestConstants.SerialNames.presentationDefinitionUri)
+    val presentationDefinitionUri: String? = null,
+
+    /**
      * OID4VP: A string containing an HTTPS URL pointing to a resource where a Presentation Definition JSON object can
      * be retrieved. This parameter MUST be present when `presentation_definition` parameter, or a `scope` value
      * representing a Presentation Definition is not present.
