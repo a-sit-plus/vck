9eb05adebb149f067312e3d8bbfec2c56a0cbd60
acrusage-iaik <139230478+acrusage-iaik@users.noreply.github.com>
2024-05-22 17:41:39 +0200

    Change: throw in private methods and use KmmResult.wrap in public methods

--- vclib-openid/src/commonMain/kotlin/at/asitplus/wallet/lib/oidc/helpers/ClientIdSchemeParametersFactory.kt
+++ vclib-openid/src/commonMain/kotlin/at/asitplus/wallet/lib/oidc/helpers/ClientIdSchemeParametersFactory.kt
@@ -1,6 +1,7 @@
 package at.asitplus.wallet.lib.oidc.helpers
 
 import at.asitplus.KmmResult
+import at.asitplus.KmmResult.Companion.wrap
 import at.asitplus.crypto.datatypes.pki.X509Certificate
 import at.asitplus.crypto.datatypes.pki.leaf
 import at.asitplus.wallet.lib.oidc.AuthenticationRequestParametersFrom
@@ -12,111 +13,90 @@ import io.ktor.http.Url
 
 class ClientIdSchemeParametersFactory(val request: AuthenticationRequestParametersFrom<*>) {
     fun createClientIdSchemeParameters(): KmmResult<ClientIdSchemeParameters?> {
-        return when (request.parameters.clientIdScheme) {
-            OpenIdConstants.ClientIdScheme.X509_SAN_DNS -> createX509SanDnsClientIdSchemeParameters()
-            OpenIdConstants.ClientIdScheme.X509_SAN_URI -> createX509SanUriClientIdSchemeParameters()
-
-            else -> createOtherClientIdSchemeParameters(request.parameters.clientIdScheme)
-        }.getOrElse {
-            return KmmResult.failure(it)
-        }.let {
-            KmmResult.success(it)
-        }
+        return KmmResult.runCatching {
+            when (request.parameters.clientIdScheme) {
+                OpenIdConstants.ClientIdScheme.X509_SAN_DNS -> createX509SanDnsClientIdSchemeParameters()
+                OpenIdConstants.ClientIdScheme.X509_SAN_URI -> createX509SanUriClientIdSchemeParameters()
+                else -> createOtherClientIdSchemeParameters(request.parameters.clientIdScheme)
+            }
+        }.wrap()
     }
 
-    private fun createOtherClientIdSchemeParameters(clientIdScheme: OpenIdConstants.ClientIdScheme?): KmmResult<ClientIdSchemeParameters.OtherClientIdSchemeParameters?> {
+    private fun createOtherClientIdSchemeParameters(clientIdScheme: OpenIdConstants.ClientIdScheme?): ClientIdSchemeParameters.OtherClientIdSchemeParameters? {
         if (request.parameters.redirectUrl != null) {
             if (request.parameters.clientId != request.parameters.redirectUrl) {
-                return KmmResult.failure(OAuth2Exception(OpenIdConstants.Errors.INVALID_REQUEST).also {
-                    Napier.w("client_id does not match redirect_uri")
-                })
+                Napier.w("client_id does not match redirect_uri")
+                throw OAuth2Exception(OpenIdConstants.Errors.INVALID_REQUEST)
             }
         }
-        return KmmResult.success(clientIdScheme?.let {
+        return clientIdScheme?.let {
             ClientIdSchemeParameters.OtherClientIdSchemeParameters(clientIdScheme)
-        })
+        }
     }
 
-    private fun createX509SanDnsClientIdSchemeParameters(): KmmResult<ClientIdSchemeParameters.X509ClientIdSchemeParameters.X509SanUriClientIdSchemeParameters> {
-        val result = validateAndRetrieveX509ClientIdSchemeParameters().getOrElse {
-            return KmmResult.failure(it)
-        }
-        return KmmResult.success(
-            ClientIdSchemeParameters.X509ClientIdSchemeParameters.X509SanUriClientIdSchemeParameters(
-                leaf = result
-            )
+    private fun createX509SanDnsClientIdSchemeParameters(): ClientIdSchemeParameters.X509ClientIdSchemeParameters.X509SanUriClientIdSchemeParameters {
+        val result = validateAndRetrieveX509ClientIdSchemeParameters()
+        return ClientIdSchemeParameters.X509ClientIdSchemeParameters.X509SanUriClientIdSchemeParameters(
+            leaf = result
         )
     }
 
-    private fun createX509SanUriClientIdSchemeParameters(): KmmResult<ClientIdSchemeParameters.X509ClientIdSchemeParameters.X509SanDnsClientIdSchemeParameters> {
-        val result = validateAndRetrieveX509ClientIdSchemeParameters().getOrElse {
-            return KmmResult.failure(it)
-        }
-        return KmmResult.success(
-            ClientIdSchemeParameters.X509ClientIdSchemeParameters.X509SanDnsClientIdSchemeParameters(
-                leaf = result
-            )
+    private fun createX509SanUriClientIdSchemeParameters(): ClientIdSchemeParameters.X509ClientIdSchemeParameters.X509SanDnsClientIdSchemeParameters {
+        val result = validateAndRetrieveX509ClientIdSchemeParameters()
+        return ClientIdSchemeParameters.X509ClientIdSchemeParameters.X509SanDnsClientIdSchemeParameters(
+            leaf = result
         )
     }
 
-    private fun validateAndRetrieveX509ClientIdSchemeParameters(): KmmResult<X509Certificate> {
+    private fun validateAndRetrieveX509ClientIdSchemeParameters(): X509Certificate {
         if (request.parameters.clientMetadata == null || request !is AuthenticationRequestParametersFrom.JwsSigned || request.source.header.certificateChain == null || request.source.header.certificateChain?.isEmpty() == true) {
-            return KmmResult.failure(OAuth2Exception(OpenIdConstants.Errors.INVALID_REQUEST).also {
-                Napier.w("client_id_scheme is ${request.parameters.clientIdScheme}, but metadata is not set and no x5c certificate chain is present in the original authn request")
-            })
+            Napier.w("client_id_scheme is ${request.parameters.clientIdScheme}, but metadata is not set and no x5c certificate chain is present in the original authn request")
+            throw OAuth2Exception(OpenIdConstants.Errors.INVALID_REQUEST)
         }
 
         val leaf = request.source.header.certificateChain!!.leaf
         if (leaf.tbsCertificate.extensions == null || leaf.tbsCertificate.extensions?.isEmpty() == true) {
-            return KmmResult.failure(OAuth2Exception(OpenIdConstants.Errors.INVALID_REQUEST).also {
-                Napier.w("client_id_scheme is ${request.parameters.clientIdScheme}, but no extensions were found in the leaf certificate")
-            })
+            Napier.w("client_id_scheme is ${request.parameters.clientIdScheme}, but no extensions were found in the leaf certificate")
+            throw OAuth2Exception(OpenIdConstants.Errors.INVALID_REQUEST)
         }
         if (request.parameters.clientIdScheme == OpenIdConstants.ClientIdScheme.X509_SAN_DNS) {
             val dnsNames = leaf.tbsCertificate.subjectAlternativeNames?.dnsNames ?: run {
-                return KmmResult.failure(OAuth2Exception(OpenIdConstants.Errors.INVALID_REQUEST).also {
-                    Napier.w("client_id_scheme is ${request.parameters.clientIdScheme}, but no dnsNames were found in the leaf certificate")
-                })
+                Napier.w("client_id_scheme is ${request.parameters.clientIdScheme}, but no dnsNames were found in the leaf certificate")
+                throw OAuth2Exception(OpenIdConstants.Errors.INVALID_REQUEST)
             }
 
             if (!dnsNames.contains(request.parameters.clientId)) {
-                return KmmResult.failure(OAuth2Exception(OpenIdConstants.Errors.INVALID_REQUEST).also {
-                    Napier.w("client_id_scheme is ${request.parameters.clientIdScheme}, but client_id does not match any dnsName in the leaf certificate")
-                })
+                Napier.w("client_id_scheme is ${request.parameters.clientIdScheme}, but client_id does not match any dnsName in the leaf certificate")
+                throw OAuth2Exception(OpenIdConstants.Errors.INVALID_REQUEST)
             }
 
             if (request.parameters.responseMode != OpenIdConstants.ResponseMode.DIRECT_POST && request.parameters.responseMode != OpenIdConstants.ResponseMode.DIRECT_POST_JWT) {
                 val parsedUrl = request.parameters.redirectUrl?.let { Url(it) } ?: run {
-                    return KmmResult.failure(OAuth2Exception(OpenIdConstants.Errors.INVALID_REQUEST).also {
-                        Napier.w("client_id_scheme is ${request.parameters.clientIdScheme}, but no redirect_url was provided")
-                    })
+                    Napier.w("client_id_scheme is ${request.parameters.clientIdScheme}, but no redirect_url was provided")
+                    throw OAuth2Exception(OpenIdConstants.Errors.INVALID_REQUEST)
                 }
 
                 //TODO  If the Wallet can establish trust in the Client Identifier authenticated through the certificate it may allow the client to freely choose the redirect_uri value
                 if (parsedUrl.host != request.parameters.clientId) {
-                    return KmmResult.failure(OAuth2Exception(OpenIdConstants.Errors.INVALID_REQUEST).also {
-                        Napier.w("client_id_scheme is ${request.parameters.clientIdScheme}, but no redirect_url was provided")
-                    })
+                    Napier.w("client_id_scheme is ${request.parameters.clientIdScheme}, but no redirect_url was provided")
+                    throw OAuth2Exception(OpenIdConstants.Errors.INVALID_REQUEST)
                 }
             }
         } else {
             val uris = leaf.tbsCertificate.subjectAlternativeNames?.uris ?: run {
-                return KmmResult.failure(OAuth2Exception(OpenIdConstants.Errors.INVALID_REQUEST).also {
-                    Napier.w("client_id_scheme is ${request.parameters.clientIdScheme}, but no URIs were found in the leaf certificate")
-                })
+                Napier.w("client_id_scheme is ${request.parameters.clientIdScheme}, but no URIs were found in the leaf certificate")
+                throw OAuth2Exception(OpenIdConstants.Errors.INVALID_REQUEST)
             }
             if (!uris.contains(request.parameters.clientId)) {
-                return KmmResult.failure(OAuth2Exception(OpenIdConstants.Errors.INVALID_REQUEST).also {
-                    Napier.w("client_id_scheme is ${request.parameters.clientIdScheme}, but client_id does not match any URIs in the leaf certificate")
-                })
+                Napier.w("client_id_scheme is ${request.parameters.clientIdScheme}, but client_id does not match any URIs in the leaf certificate")
+                throw OAuth2Exception(OpenIdConstants.Errors.INVALID_REQUEST)
             }
 
             if (request.parameters.clientId != request.parameters.redirectUrl) {
-                return KmmResult.failure(OAuth2Exception(OpenIdConstants.Errors.INVALID_REQUEST).also {
-                    Napier.w("client_id_scheme is ${request.parameters.clientIdScheme}, but client_id does not match redirect_uri")
-                })
+                Napier.w("client_id_scheme is ${request.parameters.clientIdScheme}, but client_id does not match redirect_uri")
+                throw OAuth2Exception(OpenIdConstants.Errors.INVALID_REQUEST)
             }
         }
-        return KmmResult.success(leaf)
+        return leaf
     }
 }
--- vclib-openid/src/commonMain/kotlin/at/asitplus/wallet/lib/oidc/helpers/ResponseModeParametersFactory.kt
+++ vclib-openid/src/commonMain/kotlin/at/asitplus/wallet/lib/oidc/helpers/ResponseModeParametersFactory.kt
@@ -1,6 +1,7 @@
 package at.asitplus.wallet.lib.oidc.helpers
 
 import at.asitplus.KmmResult
+import at.asitplus.KmmResult.Companion.wrap
 import at.asitplus.wallet.lib.oidc.AuthenticationRequestParameters
 import at.asitplus.wallet.lib.oidc.OpenIdConstants
 import at.asitplus.wallet.lib.oidvci.OAuth2Exception
@@ -8,7 +9,7 @@ import io.github.aakira.napier.Napier
 
 
 class ResponseModeParametersFactory(val requestParameters: AuthenticationRequestParameters) {
-    fun createResponseModeParameters(): KmmResult<ResponseModeParameters> =
+    fun createResponseModeParameters(): KmmResult<ResponseModeParameters> = KmmResult.runCatching {
         when (requestParameters.responseMode) {
             OpenIdConstants.ResponseMode.DIRECT_POST -> createDirectPostResponseModeParameters()
             OpenIdConstants.ResponseMode.DIRECT_POST_JWT -> createDirectPostJwtResponseModeParameters()
@@ -17,77 +18,56 @@ class ResponseModeParametersFactory(val requestParameters: AuthenticationRequest
             // default for vp_token and id_token is fragment
             null, OpenIdConstants.ResponseMode.FRAGMENT -> createFragmentResponseModeParameters()
             else -> TODO()
-        }.getOrElse {
-            return KmmResult.failure(it)
-        }.let {
-            KmmResult.success(it)
         }
+    }.wrap()
 
-    private fun createDirectPostResponseModeParameters(): KmmResult<ResponseModeParameters.DirectPost> {
-        val responseUrl =
-            validatePostTypeResponseModeParametersAndExtractResponseUrl().getOrElse {
-                return KmmResult.failure(it)
-            }
+    private fun createDirectPostResponseModeParameters(): ResponseModeParameters.DirectPost {
+        val responseUrl = validatePostTypeResponseModeParametersAndExtractResponseUrl()
 
-        return KmmResult.success(
-            ResponseModeParameters.DirectPost(
-                responseUrl = responseUrl
-            )
+        return ResponseModeParameters.DirectPost(
+            responseUrl = responseUrl
         )
     }
 
-    private fun createDirectPostJwtResponseModeParameters(): KmmResult<ResponseModeParameters.DirectPostJwt> {
-        val responseUrl =
-            validatePostTypeResponseModeParametersAndExtractResponseUrl().getOrElse {
-                return KmmResult.failure(it)
-            }
+    private fun createDirectPostJwtResponseModeParameters(): ResponseModeParameters.DirectPostJwt {
+        val responseUrl = validatePostTypeResponseModeParametersAndExtractResponseUrl()
 
-        return KmmResult.success(
-            ResponseModeParameters.DirectPostJwt(
-                responseUrl = responseUrl
-            )
+        return ResponseModeParameters.DirectPostJwt(
+            responseUrl = responseUrl
         )
     }
 
-    private fun validatePostTypeResponseModeParametersAndExtractResponseUrl(): KmmResult<String> {
+    private fun validatePostTypeResponseModeParametersAndExtractResponseUrl(): String {
         if (requestParameters.redirectUrl != null) {
-            return KmmResult.failure(OAuth2Exception(OpenIdConstants.Errors.INVALID_REQUEST).also {
-                Napier.w("response_mode is ${requestParameters.responseMode}, but redirect_url is set")
-            })
+            Napier.w("response_mode is ${requestParameters.responseMode}, but redirect_url is set")
+            throw OAuth2Exception(OpenIdConstants.Errors.INVALID_REQUEST)
         }
         if (requestParameters.responseUrl == null) {
-            return KmmResult.failure(OAuth2Exception(OpenIdConstants.Errors.INVALID_REQUEST).also {
-                Napier.w("response_mode is ${requestParameters.responseMode}, but response_url is not set")
-            })
+            Napier.w("response_mode is ${requestParameters.responseMode}, but response_url is not set")
+            throw OAuth2Exception(OpenIdConstants.Errors.INVALID_REQUEST)
         }
-        return KmmResult.success(requestParameters.responseUrl)
+        return requestParameters.responseUrl
     }
 
 
-    private fun createFragmentResponseModeParameters(): KmmResult<ResponseModeParameters.Fragment> {
+    private fun createFragmentResponseModeParameters(): ResponseModeParameters.Fragment {
         if (requestParameters.redirectUrl == null) {
-            return KmmResult.failure(OAuth2Exception(OpenIdConstants.Errors.INVALID_REQUEST).also {
-                Napier.w("response_mode is ${requestParameters.responseMode}, but redirect_url is not set")
-            })
+            Napier.w("response_mode is ${requestParameters.responseMode}, but redirect_url is not set")
+            throw OAuth2Exception(OpenIdConstants.Errors.INVALID_REQUEST)
         }
-        return KmmResult.success(
-            ResponseModeParameters.Fragment(
-                redirectUrl = requestParameters.redirectUrl
-            )
+        return ResponseModeParameters.Fragment(
+            redirectUrl = requestParameters.redirectUrl
         )
     }
 
 
-    private fun createQueryResponseModeParameters(): KmmResult<ResponseModeParameters.Query> {
+    private fun createQueryResponseModeParameters(): ResponseModeParameters.Query {
         if (requestParameters.redirectUrl == null) {
-            return KmmResult.failure(OAuth2Exception(OpenIdConstants.Errors.INVALID_REQUEST).also {
-                Napier.w("response_mode is ${requestParameters.responseMode}, but redirect_url is not set")
-            })
+            Napier.w("response_mode is ${requestParameters.responseMode}, but redirect_url is not set")
+            throw OAuth2Exception(OpenIdConstants.Errors.INVALID_REQUEST)
         }
-        return KmmResult.success(
-            ResponseModeParameters.Query(
-                redirectUrl = requestParameters.redirectUrl
-            )
+        return ResponseModeParameters.Query(
+            redirectUrl = requestParameters.redirectUrl
         )
     }
 }
