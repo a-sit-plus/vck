d9481b3902e46e2b363378056df95aa730f46d63
acrusage-iaik <139230478+acrusage-iaik@users.noreply.github.com>
2024-05-22 18:15:36 +0200

    Change: Make PathAuthorizationValidator a fun interface

--- vclib/src/commonMain/kotlin/at/asitplus/wallet/lib/agent/PathAuthorizationValidator.kt
+++ vclib/src/commonMain/kotlin/at/asitplus/wallet/lib/agent/PathAuthorizationValidator.kt
@@ -2,26 +2,9 @@ package at.asitplus.wallet.lib.agent
 
 import at.asitplus.jsonpath.core.NormalizedJsonPath
 
-interface PathAuthorizationValidator {
+fun interface PathAuthorizationValidator {
     operator fun invoke(
         credential: SubjectCredentialStore.StoreEntry,
         attributePath: NormalizedJsonPath
     ): Boolean
-}
-
-class LambdaPathAuthorizationValidator(
-    val evaluator: (
-        credential: SubjectCredentialStore.StoreEntry,
-        attributePath: NormalizedJsonPath,
-    ) -> Boolean
-) : PathAuthorizationValidator {
-    override operator fun invoke(
-        credential: SubjectCredentialStore.StoreEntry,
-        attributePath: NormalizedJsonPath
-    ): Boolean {
-        return evaluator(
-            credential,
-            attributePath,
-        )
-    }
 }
