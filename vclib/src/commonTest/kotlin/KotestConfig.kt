3bcd09e1edd728d8207b41b8754bd892e461d427
Bernd Prünster <bernd.pruenster@a-sit.at>
2024-06-06 17:47:31 +0200

    fix KotextConfig

--- vclib/src/commonTest/kotlin/KotestConfig.kt
+++ vclib/src/commonTest/kotlin/KotestConfig.kt
@@ -2,7 +2,7 @@ import io.github.aakira.napier.DebugAntilog
 import io.github.aakira.napier.Napier
 import io.kotest.core.config.AbstractProjectConfig
 
-object KotestConfig : AbstractProjectConfig() {
+class KotestConfig : AbstractProjectConfig() {
     init {
         Napier.base(DebugAntilog())
     }
