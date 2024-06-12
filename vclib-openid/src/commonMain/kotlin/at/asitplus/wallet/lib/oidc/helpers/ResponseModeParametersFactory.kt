9670de2a343024119e709016f74bb7cb8bcc9061
acrusage-iaik <139230478+acrusage-iaik@users.noreply.github.com>
2024-06-12 18:27:30 +0200

    Refactor: Simplify code

--- vclib-openid/src/commonMain/kotlin/at/asitplus/wallet/lib/oidc/helpers/ResponseModeParametersFactory.kt
+++ vclib-openid/src/commonMain/kotlin/at/asitplus/wallet/lib/oidc/helpers/ResponseModeParametersFactory.kt
@@ -17,58 +17,48 @@ internal object ResponseModeParametersFactory {
             null, // default for vp_token and id_token is fragment
             OpenIdConstants.ResponseMode.FRAGMENT -> createFragmentResponseModeParameters(request.parameters)
 
-            OpenIdConstants.ResponseMode.DIRECT_POST -> createDirectPostResponseModeParameters(request.parameters)
+            OpenIdConstants.ResponseMode.DIRECT_POST -> createDirectPostResponseModeParameters(
+                request
+            )
+
             OpenIdConstants.ResponseMode.DIRECT_POST_JWT -> createDirectPostJwtResponseModeParameters(
                 request,
             )
+
             OpenIdConstants.ResponseMode.QUERY -> createQueryResponseModeParameters(request.parameters)
 
             is OpenIdConstants.ResponseMode.OTHER -> TODO()
         }
     }.wrap()
 
-    private fun createDirectPostResponseModeParameters(requestParameters: AuthenticationRequestParameters): ResponseModeParameters.DirectPost {
-        val responseUrl = validatePostTypeResponseModeParametersAndExtractResponseUrl(requestParameters)
-
-        return ResponseModeParameters.DirectPost(
-            responseUrl = responseUrl
+    private fun createDirectPostResponseModeParameters(request: AuthenticationRequest) =
+        ResponseModeParameters.DirectPost(
+            responseUrl = validatePostTypeResponseModeParametersAndExtractResponseUrl(request.parameters),
         )
-    }
 
-    private fun createDirectPostJwtResponseModeParameters(
-        request: AuthenticationRequest,
-    ): ResponseModeParameters.DirectPostJwt {
-        val responseUrl = validatePostTypeResponseModeParametersAndExtractResponseUrl(request.parameters)
 
-        return ResponseModeParameters.DirectPostJwt(
-            responseUrl = responseUrl,
+    private fun createDirectPostJwtResponseModeParameters(request: AuthenticationRequest) =
+        ResponseModeParameters.DirectPostJwt(
+            responseUrl = validatePostTypeResponseModeParametersAndExtractResponseUrl(request.parameters),
         )
-    }
 
 
-    private fun createFragmentResponseModeParameters(requestParameters: AuthenticationRequestParameters): ResponseModeParameters.Fragment {
-        if (requestParameters.redirectUrl == null) {
-            Napier.w("response_mode is ${requestParameters.responseMode}, but redirect_url is not set")
-            throw OAuth2Exception(OpenIdConstants.Errors.INVALID_REQUEST)
-        }
-        return ResponseModeParameters.Fragment(
-            redirectUrl = requestParameters.redirectUrl
+    private fun createFragmentResponseModeParameters(request: AuthenticationRequest) =
+        ResponseModeParameters.Fragment(
+            redirectUrl = request.parameters.redirectUrl ?: run {
+                Napier.w("response_mode is ${request.parameters.responseMode}, but redirect_url is not set")
+                throw OAuth2Exception(OpenIdConstants.Errors.INVALID_REQUEST)
+            },
         )
-    }
 
 
-    private fun createQueryResponseModeParameters(requestParameters: AuthenticationRequestParameters): ResponseModeParameters.Query {
-        if (requestParameters.redirectUrl == null) {
-            Napier.w("response_mode is ${requestParameters.responseMode}, but redirect_url is not set")
-            throw OAuth2Exception(OpenIdConstants.Errors.INVALID_REQUEST)
-        }
-        return ResponseModeParameters.Query(
-            redirectUrl = requestParameters.redirectUrl
+    private fun createQueryResponseModeParameters(request: AuthenticationRequest) =
+        ResponseModeParameters.Query(
+            redirectUrl = request.parameters.redirectUrl ?: run {
+                Napier.w("response_mode is ${request.parameters.responseMode}, but redirect_url is not set")
+                throw OAuth2Exception(OpenIdConstants.Errors.INVALID_REQUEST)
+            },
         )
-    }
-
-
-
 
 
     private fun validatePostTypeResponseModeParametersAndExtractResponseUrl(requestParameters: AuthenticationRequestParameters): String {
