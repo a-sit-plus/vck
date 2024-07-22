45198dd3b1dca4a738b52c9013f0ebd9880d2b56
Christian Kollmann <christian.kollmann@a-sit.at>
2024-07-22 08:25:47 +0200

    Use expression body

--- vclib/src/commonMain/kotlin/at/asitplus/wallet/lib/data/AttributeIndex.kt
+++ vclib/src/commonMain/kotlin/at/asitplus/wallet/lib/data/AttributeIndex.kt
@@ -1,65 +1,65 @@
 package at.asitplus.wallet.lib.data
 
+import at.asitplus.wallet.lib.data.ConstantIndex.CredentialRepresentation
+import at.asitplus.wallet.lib.data.ConstantIndex.CredentialScheme
+
 object AttributeIndex {
 
-    var schemeSet = setOf<ConstantIndex.CredentialScheme>()
+    var schemeSet = setOf<CredentialScheme>()
         private set
 
     init {
         schemeSet += ConstantIndex.AtomicAttribute2023
     }
 
-    internal fun registerAttributeType(scheme: ConstantIndex.CredentialScheme) {
+    internal fun registerAttributeType(scheme: CredentialScheme) {
         schemeSet += scheme
     }
 
     /**
-     * Matches the passed [uri] against all known schemes from [ConstantIndex.CredentialScheme.schemaUri]
+     * Matches the passed [uri] against all known schemes from [ConstantIndex.CredentialScheme.schemaUri].
      */
-    fun resolveSchemaUri(uri: String): ConstantIndex.CredentialScheme? {
-        return schemeSet.firstOrNull { it.schemaUri == uri }
-    }
+    fun resolveSchemaUri(uri: String): CredentialScheme? =
+        schemeSet.firstOrNull { it.schemaUri == uri }
 
     /**
-     * Matches the passed [type] against all known types from [ConstantIndex.CredentialScheme.vcType]
+     * Matches the passed [type] against all known types from [ConstantIndex.CredentialScheme.vcType].
      */
-    fun resolveAttributeType(type: String): ConstantIndex.CredentialScheme? {
-        return schemeSet.firstOrNull { it.vcType == type }
-    }
+    fun resolveAttributeType(type: String): CredentialScheme? =
+        schemeSet.firstOrNull { it.vcType == type }
 
     /**
-     * Matches the passed [sdJwtType] against all known types from [ConstantIndex.CredentialScheme.sdJwtType]
+     * Matches the passed [sdJwtType] against all known types from [ConstantIndex.CredentialScheme.sdJwtType].
      */
-    fun resolveSdJwtAttributeType(sdJwtType: String): ConstantIndex.CredentialScheme? {
-        return schemeSet.firstOrNull { it.sdJwtType == sdJwtType }
-    }
+    fun resolveSdJwtAttributeType(sdJwtType: String): CredentialScheme? =
+        schemeSet.firstOrNull { it.sdJwtType == sdJwtType }
 
     /**
-     * Matches the passed [namespace] against all known namespace from [ConstantIndex.CredentialScheme.isoNamespace]
+     * Matches the passed [namespace] against all known namespace from [ConstantIndex.CredentialScheme.isoNamespace].
+     *
+     * Allows for extension to the namespace by appending ".countryname" or anything else, according to spec.
      */
-    fun resolveIsoNamespace(namespace: String): ConstantIndex.CredentialScheme? {
-        // allow for extension to the namespace by appending ".countryname" or anything else, according to spec
-        return schemeSet.filter { it.isoNamespace != null }
+    fun resolveIsoNamespace(namespace: String): CredentialScheme? =
+        schemeSet.filter { it.isoNamespace != null }
             .firstOrNull { it.isoNamespace!!.startsWith(namespace) || namespace.startsWith(it.isoNamespace!!) }
-    }
 
     /**
-     * Matches the passed [docType] against all known docTypes from [ConstantIndex.CredentialScheme.isoDocType]
+     * Matches the passed [docType] against all known docTypes from [ConstantIndex.CredentialScheme.isoDocType].
+     *
+     * Allows for extension to the namespace by appending ".countryname" or anything else, according to spec.
      */
-    fun resolveIsoDoctype(docType: String): ConstantIndex.CredentialScheme? {
-        // allow for extension to the namespace by appending ".countryname" or anything else, according to spec
-        return schemeSet.filter { it.isoDocType != null }
+    fun resolveIsoDoctype(docType: String): CredentialScheme? =
+        schemeSet.filter { it.isoDocType != null }
             .firstOrNull { it.isoDocType!!.startsWith(docType) || docType.startsWith(it.isoDocType!!) }
-    }
 
     /**
-     * Compares the input to all CredentialScheme identifiers and on match returns it plus its associated `ConstantIndex.CredentialRepresentation` if applicable
+     * Compares the input to all [CredentialScheme] identifiers and on match
+     * returns it plus its associated [CredentialRepresentation] if applicable.
      */
-    fun resolveCredential(input: String): Pair<ConstantIndex.CredentialScheme, ConstantIndex.CredentialRepresentation?>? {
-        return resolveAttributeType(input)?.let { it to ConstantIndex.CredentialRepresentation.PLAIN_JWT}
-            ?: resolveSdJwtAttributeType(input)?.let { it to ConstantIndex.CredentialRepresentation.SD_JWT}
-            ?: resolveIsoNamespace(input)?.let { it to ConstantIndex.CredentialRepresentation.ISO_MDOC}
-            ?: resolveIsoDoctype(input)?.let { it to ConstantIndex.CredentialRepresentation.ISO_MDOC}
-            ?: resolveSchemaUri(input)?.let { it to null}
-    }
+    fun resolveCredential(input: String): Pair<CredentialScheme, CredentialRepresentation?>? =
+        resolveAttributeType(input)?.let { it to CredentialRepresentation.PLAIN_JWT }
+            ?: resolveSdJwtAttributeType(input)?.let { it to CredentialRepresentation.SD_JWT }
+            ?: resolveIsoNamespace(input)?.let { it to CredentialRepresentation.ISO_MDOC }
+            ?: resolveIsoDoctype(input)?.let { it to CredentialRepresentation.ISO_MDOC }
+            ?: resolveSchemaUri(input)?.let { it to null }
 }
