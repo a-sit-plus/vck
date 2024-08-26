07ba7ae9dc13b765cebc9370c91e0b269bde75ef
Christian Kollmann <christian.kollmann@a-sit.at>
2024-08-26 13:42:18 +0200

    Externalize map store

---
+++ vck-openid/src/commonMain/kotlin/at/asitplus/wallet/lib/oidvci/MapStore.kt
@@ -0,0 +1,29 @@
+package at.asitplus.wallet.lib.oidvci
+
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
+
+interface MapStore<T, U> {
+
+    suspend fun put(key: T, value: U)
+
+    suspend fun get(key: T): U?
+
+}
+
+
+/**
+ * Holds map in memory, protected with a [Mutex]
+ */
+class DefaultMapStore<T, U> : MapStore<T, U> {
+
+    private val mutex = Mutex()
+    private val map = mutableMapOf<T, U>()
+
+    override suspend fun put(key: T, value: U) {
+        mutex.withLock { map.put(key, value) }
+    }
+
+    override suspend fun get(key: T) = mutex.withLock { map[key] }
+
+}
