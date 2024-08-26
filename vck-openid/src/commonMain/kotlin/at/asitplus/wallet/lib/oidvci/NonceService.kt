14fbf27d8a6ff4a44a50168434d57f27056f9fdc
Christian Kollmann <christian.kollmann@a-sit.at>
2024-08-26 09:37:08 +0200

    Add mutex to support basic concurrency

--- vck-openid/src/commonMain/kotlin/at/asitplus/wallet/lib/oidvci/NonceService.kt
+++ vck-openid/src/commonMain/kotlin/at/asitplus/wallet/lib/oidvci/NonceService.kt
@@ -1,24 +1,30 @@
 package at.asitplus.wallet.lib.oidvci
 
 import com.benasher44.uuid.uuid4
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
 interface NonceService {
 
-    fun provideNonce(): String
+    suspend fun provideNonce(): String
 
-    fun verifyAndRemoveNonce(it: String): Boolean
+    suspend fun verifyAndRemoveNonce(it: String): Boolean
 
 }
 
+/**
+ * Holds nonces in memory, protected with a [Mutex].
+ */
 class DefaultNonceService : NonceService {
 
+    private val mutex = Mutex()
     private val validNonces = mutableListOf<String>()
 
-    override fun provideNonce(): String {
-        return uuid4().toString().also { validNonces += it }
+    override suspend fun provideNonce(): String {
+        return uuid4().toString().also { mutex.withLock { validNonces += it } }
     }
 
-    override fun verifyAndRemoveNonce(it: String): Boolean {
-        return validNonces.remove(it)
+    override suspend fun verifyAndRemoveNonce(it: String): Boolean {
+        return mutex.withLock { validNonces.remove(it) }
     }
 }
