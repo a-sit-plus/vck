822358ddafe80ecfb6f0df74646de907b50ec70b
Simon Mueller <simon.mueller@a-sit.at>
2024-09-13 16:28:33 +0200

    Add RqesRequest

--- dif-data-classes/src/commonMain/kotlin/at/asitplus/dif/rqes/RqesConstants.kt
+++ dif-data-classes/src/commonMain/kotlin/at/asitplus/dif/rqes/RqesConstants.kt
@@ -1,9 +1,6 @@
 package at.asitplus.dif.rqes
 
 object RqesConstants {
-
+    const val RESPONSE_TYPE = "code"
     const val SCOPE = "credential"
-
-    const val SIGNATURE_QUALIFIER = "eu_eidas_qes"
-
 }
--- vck-openid/src/commonMain/kotlin/at/asitplus/wallet/lib/oidvci/RqesVerifier.kt
+++
@@ -1,5 +0,0 @@
-package at.asitplus.wallet.lib.oidvci
-
-class RqesVerifier {
-
-}
