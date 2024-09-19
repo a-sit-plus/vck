283b7ffc5f29acd8875044bed7ec0849f34eab0d
Simon Mueller <simon.mueller@a-sit.at>
2024-09-19 14:31:15 +0200

    Extend RqesWalletService

--- vck-openid/src/commonMain/kotlin/at/asitplus/wallet/lib/oidvci/RqesWalletService.kt
+++ vck-openid/src/commonMain/kotlin/at/asitplus/wallet/lib/oidvci/RqesWalletService.kt
@@ -4,6 +4,7 @@ import at.asitplus.dif.rqes.Enums.SignatureFormat
 import at.asitplus.dif.rqes.Enums.SignatureQualifierEnum
 import at.asitplus.dif.rqes.RqesConstants
 import at.asitplus.dif.rqes.SignDocParameters
+import at.asitplus.dif.rqes.SignHashParameters
 import at.asitplus.dif.rqes.SignatureRequestParameters
 import at.asitplus.openid.AuthenticationRequestParameters
 import at.asitplus.openid.AuthorizationDetails
@@ -126,9 +127,10 @@ class RqesWalletService(
     }
 
     /**
-     * TODO: could also use [Document] instead of [CscDocumentDigest]
+     * TODO: could also use [Document] instead of [CscDocumentDigest], also [credential_id] instead of [SAD]
      */
-    suspend fun createSignDocRequestParameters(rqesRequest: RqesRequest): SignatureRequestParameters = SignDocParameters(
+    suspend fun createSignDocRequestParameters(rqesRequest: RqesRequest, sad: String): SignatureRequestParameters = SignDocParameters(
+        sad = sad,
         signatureQualifier = rqesRequest.signatureQualifier,
         documentDigests = listOf(rqesRequest.getCscDocumentDigests(
             signatureFormat = SignatureFormat.CADES,
@@ -137,7 +139,10 @@ class RqesWalletService(
         responseUri = this.redirectUrl, //TODO double check
     )
 
-    suspend fun createSignHashRequestParameters(rqesRequest: RqesRequest): SignatureRequestParameters = TODO()
+    suspend fun createSignHashRequestParameters(rqesRequest: RqesRequest, sad: String): SignatureRequestParameters = SignHashParameters(
+        credentialId = rqesRequest.clientId,
+        hashes = rqesRequest.documentDigests.map { it.hash }
+    )
 
 }
 
