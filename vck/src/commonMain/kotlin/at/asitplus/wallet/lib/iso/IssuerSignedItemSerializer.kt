5ee9f809cf79abaeddb25f0232de1e252f206fcd
Christian Kollmann <christian.kollmann@a-sit.at>
2024-09-24 12:44:01 +0200

    Refactor code

--- vck/src/commonMain/kotlin/at/asitplus/wallet/lib/iso/IssuerSignedItemSerializer.kt
+++ vck/src/commonMain/kotlin/at/asitplus/wallet/lib/iso/IssuerSignedItemSerializer.kt
@@ -1,6 +1,10 @@
 package at.asitplus.wallet.lib.iso
 
 import at.asitplus.wallet.lib.data.InstantStringSerializer
+import at.asitplus.wallet.lib.iso.IssuerSignedItem.Companion.PROP_DIGEST_ID
+import at.asitplus.wallet.lib.iso.IssuerSignedItem.Companion.PROP_ELEMENT_ID
+import at.asitplus.wallet.lib.iso.IssuerSignedItem.Companion.PROP_ELEMENT_VALUE
+import at.asitplus.wallet.lib.iso.IssuerSignedItem.Companion.PROP_RANDOM
 import io.github.aakira.napier.Napier
 import kotlinx.datetime.Instant
 import kotlinx.datetime.LocalDate
@@ -15,10 +19,10 @@ import kotlinx.serialization.encoding.*
 open class IssuerSignedItemSerializer(private val namespace: String) : KSerializer<IssuerSignedItem> {
 
     override val descriptor: SerialDescriptor = buildClassSerialDescriptor("IssuerSignedItem") {
-        element(IssuerSignedItem.PROP_DIGEST_ID, Long.serializer().descriptor)
-        element(IssuerSignedItem.PROP_RANDOM, ByteArraySerializer().descriptor)
-        element(IssuerSignedItem.PROP_ELEMENT_ID, String.serializer().descriptor)
-        element(IssuerSignedItem.PROP_ELEMENT_VALUE, String.serializer().descriptor)
+        element(PROP_DIGEST_ID, Long.serializer().descriptor)
+        element(PROP_RANDOM, ByteArraySerializer().descriptor)
+        element(PROP_ELEMENT_ID, String.serializer().descriptor)
+        element(PROP_ELEMENT_VALUE, String.serializer().descriptor)
     }
 
     override fun serialize(encoder: Encoder, value: IssuerSignedItem) {
@@ -32,15 +36,13 @@ open class IssuerSignedItemSerializer(private val namespace: String) : KSerializ
 
     private fun CompositeEncoder.encodeAnything(value: IssuerSignedItem, index: Int) {
         val descriptor = buildClassSerialDescriptor("IssuerSignedItem") {
-            element(IssuerSignedItem.PROP_DIGEST_ID, Long.serializer().descriptor)
-            element(IssuerSignedItem.PROP_RANDOM, ByteArraySerializer().descriptor)
-            element(IssuerSignedItem.PROP_ELEMENT_ID, String.serializer().descriptor)
+            element(PROP_DIGEST_ID, Long.serializer().descriptor)
+            element(PROP_RANDOM, ByteArraySerializer().descriptor)
+            element(PROP_ELEMENT_ID, String.serializer().descriptor)
             element(
-                elementName = IssuerSignedItem.PROP_ELEMENT_VALUE,
+                elementName = PROP_ELEMENT_VALUE,
                 descriptor = buildElementValueSerializer(value.elementValue).descriptor,
-                annotations = if (value.elementValue is LocalDate || value.elementValue is Instant)
-                    @OptIn(ExperimentalUnsignedTypes::class)
-                    listOf(ValueTags(1004uL)) else emptyList()
+                annotations = value.elementValue.annotations()
             )
         }
 
@@ -54,6 +56,14 @@ open class IssuerSignedItemSerializer(private val namespace: String) : KSerializ
             else -> CborCredentialSerializer.encode(descriptor, index, this, it)
         }
     }
+
+    private fun Any.annotations() =
+        if (this is LocalDate || this is Instant) {
+            @OptIn(ExperimentalUnsignedTypes::class)
+            listOf(ValueTags(1004uL))
+        } else {
+            emptyList()
+        }
 
     private inline fun <reified T> buildElementValueSerializer(element: T) = when (element) {
         is String -> String.serializer()
@@ -75,15 +85,13 @@ open class IssuerSignedItemSerializer(private val namespace: String) : KSerializ
         decoder.decodeStructure(descriptor) {
             while (true) {
                 val name = decodeStringElement(descriptor, 0)
-                val index =
-                    descriptor.getElementIndex(name) //Don't call decodeElementIndex, as it would check for tags. this would break decodeAnything
+                // Don't call decodeElementIndex, as it would check for tags. this would break decodeAnything
+                val index = descriptor.getElementIndex(name)
                 when (name) {
-                    IssuerSignedItem.PROP_DIGEST_ID -> digestId = decodeLongElement(descriptor, index).toUInt()
-                    IssuerSignedItem.PROP_RANDOM -> random =
-                        decodeSerializableElement(descriptor, index, ByteArraySerializer())
-
-                    IssuerSignedItem.PROP_ELEMENT_ID -> elementIdentifier = decodeStringElement(descriptor, index)
-                    IssuerSignedItem.PROP_ELEMENT_VALUE -> elementValue = decodeAnything(index, elementIdentifier)
+                    PROP_DIGEST_ID -> digestId = decodeLongElement(descriptor, index).toUInt()
+                    PROP_RANDOM -> random = decodeSerializableElement(descriptor, index, ByteArraySerializer())
+                    PROP_ELEMENT_ID -> elementIdentifier = decodeStringElement(descriptor, index)
+                    PROP_ELEMENT_VALUE -> elementValue = decodeAnything(index, elementIdentifier)
                 }
                 if (index == 3) break
             }
@@ -99,19 +107,20 @@ open class IssuerSignedItemSerializer(private val namespace: String) : KSerializ
     private fun CompositeDecoder.decodeAnything(index: Int, elementIdentifier: String): Any {
         if (namespace.isBlank()) Napier.w { "This decoder is not namespace-aware! Unspeakable things may happen…" }
 
-        //Tags are not read out here but skipped because `decodeElementIndex` is never called, so we cannot discriminate
-        //technically, this should be a good thing though, because otherwise we'd consume more from the input
+        // Tags are not read out here but skipped because `decodeElementIndex` is never called, so we cannot
+        // discriminate technically, this should be a good thing though, because otherwise we'd consume more from the
+        // input
         runCatching {
-
-            CborCredentialSerializer.decode(descriptor, index, this, elementIdentifier, namespace)?.let {
-                return it
-            }
-                ?: Napier.w { "Could not find a registered decoder for namespace $namespace and elementIdentifier $elementIdentifier. Falling back to defaults" }
-
+            CborCredentialSerializer.decode(descriptor, index, this, elementIdentifier, namespace)
+                ?.let { return it }
+                ?: Napier.w {
+                    "Could not find a registered decoder for namespace $namespace and elementIdentifier" +
+                            " $elementIdentifier. Falling back to defaults"
+                }
         }
 
-        //These are the ones that map to different CBOR data types, the rest don't, so if it is not registered, we'll lose type information
-        //No others must be added here, as they could consume data from the underlying bytes
+        // These are the ones that map to different CBOR data types, the rest don't, so if it is not registered, we'll
+        // lose type information. No others must be added here, as they could consume data from the underlying bytes
         runCatching { return decodeStringElement(descriptor, index) }
         runCatching { return decodeLongElement(descriptor, index) }
         runCatching { return decodeDoubleElement(descriptor, index) }
