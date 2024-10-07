738b841157779c4d24ce8ebb778d1ed01a51c167
Simon Mueller <simon.mueller@a-sit.at>
2024-10-07 13:42:51 +0200

    Clean up

--- vck-openid/src/commonMain/kotlin/at/asitplus/wallet/lib/oidc/RequestParameterFrom.kt
+++ vck-openid/src/commonMain/kotlin/at/asitplus/wallet/lib/oidc/RequestParameterFrom.kt
@@ -1,26 +1,11 @@
 package at.asitplus.wallet.lib.oidc
 
 import at.asitplus.openid.RequestParameters
-import kotlinx.serialization.DeserializationStrategy
+import at.asitplus.openid.RequestParametersSerializer
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.JsonContentPolymorphicSerializer
-import kotlinx.serialization.json.JsonElement
-import kotlinx.serialization.json.jsonObject
 
 @Serializable
 sealed interface RequestParametersFrom {
+    @Serializable(with  = RequestParametersSerializer::class)
     val parameters: RequestParameters
-}
-
-object RequestParametersFromSerializer :
-    JsonContentPolymorphicSerializer<RequestParametersFrom>(RequestParametersFrom::class) {
-    override fun selectDeserializer(element: JsonElement): DeserializationStrategy<RequestParametersFrom> {
-        val parameters = element.jsonObject["parameters"]?.jsonObject
-        return parameters?.let {
-            when {
-                "signatureQualifier" in it -> SignatureRequestParametersFrom.serializer()
-                else -> AuthenticationRequestParametersFrom.serializer()
-            }
-        } ?: throw Exception("Invalid parameters")
-    }
 }
