489d3c135ede5f8eb0a876e7812a39df57964879
Christian Kollmann <christian.kollmann@a-sit.at>
2024-10-29 10:05:50 +0100

    Refactor code

--- vck/src/commonMain/kotlin/at/asitplus/wallet/lib/data/CredentialToJsonConverter.kt
+++ vck/src/commonMain/kotlin/at/asitplus/wallet/lib/data/CredentialToJsonConverter.kt
@@ -8,57 +8,55 @@ import io.matthewnelson.encoding.core.Encoder.Companion.encodeToString
 import kotlinx.datetime.LocalDate
 import kotlinx.serialization.json.*
 
+/**
+ * In OpenID4VP, the claims to be presented are described using a JSONPath, so compiling this to a JsonElement seems
+ * reasonable.
+ */
 object CredentialToJsonConverter {
-    // in openid4vp, the claims to be presented are described using a JSONPath, so compiling this to a JsonElement seems reasonable
-    fun toJsonElement(credential: SubjectCredentialStore.StoreEntry): JsonElement {
-        return when (credential) {
-            is SubjectCredentialStore.StoreEntry.Vc -> {
-                buildJsonObject {
-                    put("type", JsonPrimitive(credential.scheme.vcType))
-                    vckJsonSerializer.encodeToJsonElement(credential.vc.vc.credentialSubject).jsonObject.entries.forEach {
-                        put(it.key, it.value)
-                    }
-                    // TODO: Remove the rest here when there is a clear specification on how to encode vc credentials
-                    //  This may actually depend on the presentation context, so more information may be required
-                    put("vc", buildJsonArray {
-                        add(vckJsonSerializer.encodeToJsonElement(credential.vc.vc.credentialSubject))
-                    })
+
+    /**
+     * The result is used in [at.asitplus.wallet.lib.data.dif.InputEvaluator.evaluateConstraintFieldMatches]
+     */
+    fun toJsonElement(credential: SubjectCredentialStore.StoreEntry): JsonElement = when (credential) {
+        is SubjectCredentialStore.StoreEntry.Vc -> buildJsonObject {
+            put("type", JsonPrimitive(credential.scheme.vcType))
+            val vcAsJsonElement = vckJsonSerializer.encodeToJsonElement(credential.vc.vc.credentialSubject)
+            vcAsJsonElement.jsonObject.entries.forEach {
+                put(it.key, it.value)
+            }
+            // TODO: Remove the rest here when there is a clear specification on how to encode vc credentials
+            //  This may actually depend on the presentation context, so more information may be required
+            put("vc", buildJsonArray {
+                add(vcAsJsonElement)
+            })
+        }
+
+        is SubjectCredentialStore.StoreEntry.SdJwt -> {
+            val sdJwtSigned = SdJwtSigned.parse(credential.vcSerialized)
+            val payloadVc = sdJwtSigned?.getPayloadAsJsonObject()?.getOrNull()
+            val reconstructed = sdJwtSigned?.let { SdJwtValidator(it).reconstructedJsonObject }
+            val simpleDisclosureMap = credential.disclosures.map { entry ->
+                entry.value?.let { it.claimName to it.claimValue }
+            }.filterNotNull().toMap()
+
+            buildJsonObject {
+                put("vct", JsonPrimitive(credential.scheme.sdJwtType ?: credential.scheme.vcType))
+                payloadVc?.forEach { put(it.key, it.value) }
+                reconstructed?.forEach {
+                    put(it.key, it.value)
+                } ?: simpleDisclosureMap.forEach { pair ->
+                    pair.key?.let { put(it, pair.value) }
                 }
             }
+        }
 
-            is SubjectCredentialStore.StoreEntry.SdJwt -> {
-                val sdJwtSigned = SdJwtSigned.parse(credential.vcSerialized)
-                val payloadVc = sdJwtSigned?.getPayloadAsJsonObject()?.getOrNull()
-                val reconstructed = sdJwtSigned?.let { SdJwtValidator(it).reconstructedJsonObject }
-                val simpleDisclosureMap = credential.disclosures.map { entry ->
-                    entry.value?.let { it.claimName to it.claimValue }
-                }.filterNotNull().toMap()
-
-
-                buildJsonObject {
-                    put("vct", JsonPrimitive(credential.scheme.sdJwtType ?: credential.scheme.vcType))
-                    payloadVc?.forEach { put(it.key, it.value) }
-                    reconstructed?.forEach {
-                        put(it.key, it.value)
-                    } ?: simpleDisclosureMap.forEach { pair ->
-                        pair.key?.let { put(it, pair.value) }
+        is SubjectCredentialStore.StoreEntry.Iso -> buildJsonObject {
+            credential.issuerSigned.namespaces?.forEach {
+                put(it.key, buildJsonObject {
+                    it.value.entries.map { it.value }.forEach { value ->
+                        put(value.elementIdentifier, value.elementValue.toJsonElement())
                     }
-                }
-            }
-
-            is SubjectCredentialStore.StoreEntry.Iso -> {
-                buildJsonObject {
-                    credential.issuerSigned.namespaces?.forEach {
-                        put(it.key, buildJsonObject {
-                            it.value.entries.forEach { signedItem ->
-                                put(
-                                    signedItem.value.elementIdentifier,
-                                    signedItem.value.elementValue.toJsonElement()
-                                )
-                            }
-                        })
-                    }
-                }
+                })
             }
         }
     }
