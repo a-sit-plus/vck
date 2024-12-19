d6c2c7b8f10dfc0cc711c651f3b3da6859d9c671
Simon Mueller <simon.mueller@a-sit.at>
2024-12-19 11:11:22 +0100

    Add documentation

--- rqes-data-classes/src/commonMain/kotlin/at/asitplus/rqes/collection_entries/CscKeyParameters.kt
+++ rqes-data-classes/src/commonMain/kotlin/at/asitplus/rqes/collection_entries/CscKeyParameters.kt
@@ -1,24 +1,47 @@
 package at.asitplus.rqes.collection_entries
 
+import at.asitplus.signum.indispensable.asn1.ObjectIdentifier
 import kotlinx.serialization.SerialName
 
 data class CscKeyParameters(
+    /**
+     * The status of the signing key of the credential:
+     */
     @SerialName("status")
     val status: KeyStatusOptions,
 
+    /**
+     * The list of OIDs of the supported key algorithms
+     */
     @SerialName("algo")
-    val algo: Collection<String>,
+    val algo: Collection<ObjectIdentifier>,
 
+    /**
+     * The length of the cryptographic key in bits.
+     */
     @SerialName("len")
     val len: UInt,
 
+    /**
+     * The OID of the ECDSA curve. The value SHALL only be returned if
+     * [algo] is based on ECDSA.
+     */
     @SerialName("curve")
-    val curve: String? = null,
+    val curve: ObjectIdentifier? = null,
 ) {
     enum class KeyStatusOptions {
+        /**
+         * the signing key is enabled and can be used for signing.
+         */
         @SerialName("enabled")
         ENABLED,
 
+        /**
+         * the signing key is disabled and cannot be used for
+         * signing. This MAY occur when the owner has disabled it or when
+         * the RSSP has detected that the associated certificate is expired or
+         * revoked.
+         */
         @SerialName("disabled")
         DISABLED
     }
