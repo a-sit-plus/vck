87da10e9b06f5c6273377091b9b5f24fd39c5e45
Christian Kollmann <christian.kollmann@a-sit.at>
2025-01-28 13:23:10 +0100

    Extract dummy value provider

--- vck-rqes/src/commonTest/kotlin/at/asitplus/wallet/lib/rqes/RqesOpenId4VpHolderTest.kt
+++ vck-rqes/src/commonTest/kotlin/at/asitplus/wallet/lib/rqes/RqesOpenId4VpHolderTest.kt
@@ -8,16 +8,15 @@ import at.asitplus.openid.SignatureQualifier
 import at.asitplus.openid.TokenRequestParameters
 import at.asitplus.rqes.CredentialInfo
 import at.asitplus.rqes.SignHashParameters
-import at.asitplus.rqes.collection_entries.*
+import at.asitplus.rqes.collection_entries.CscCertificateParameters
+import at.asitplus.rqes.collection_entries.CscDocumentDigest
+import at.asitplus.rqes.collection_entries.CscKeyParameters
 import at.asitplus.rqes.enums.ConformanceLevel
 import at.asitplus.rqes.enums.SignatureFormat
 import at.asitplus.rqes.serializers.CscSignatureRequestParameterSerializer
 import at.asitplus.signum.indispensable.Digest
-import at.asitplus.signum.indispensable.X509SignatureAlgorithm
 import at.asitplus.signum.indispensable.X509SignatureAlgorithm.entries
 import at.asitplus.signum.indispensable.io.Base64UrlStrict
-import at.asitplus.signum.indispensable.josef.toJwsAlgorithm
-import at.asitplus.wallet.lib.agent.EphemeralKeyWithSelfSignedCert
 import at.asitplus.wallet.lib.data.vckJsonSerializer
 import at.asitplus.wallet.lib.oauth2.OAuth2Client
 import com.benasher44.uuid.bytes
@@ -25,59 +24,11 @@ import com.benasher44.uuid.uuid4
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.shouldNotBe
-import io.matthewnelson.encoding.core.Decoder.Companion.decodeToByteArray
 import io.matthewnelson.encoding.core.Encoder.Companion.encodeToString
 import kotlinx.serialization.PolymorphicSerializer
-import kotlin.random.Random
 
 class RqesOpenId4VpHolderTest : FreeSpec({
 
-    class DummyValueProvider {
-        val viableSigAlg = listOf(
-            X509SignatureAlgorithm.RS256,
-            X509SignatureAlgorithm.RS384,
-            X509SignatureAlgorithm.RS512,
-            X509SignatureAlgorithm.ES256,
-            X509SignatureAlgorithm.ES384,
-            X509SignatureAlgorithm.ES512,
-        )
-
-        suspend fun getSigningCredential(isValid: Boolean = false): CredentialInfo {
-            val signatureAlgo = viableSigAlg.random()
-
-            return CredentialInfo(
-                credentialID = uuid4().toString(),
-                signatureQualifier = SignatureQualifier.EU_EIDAS_QES,
-                keyParameters = CscKeyParameters(
-                    status = if (isValid) CscKeyParameters.KeyStatusOptions.ENABLED else CscKeyParameters.KeyStatusOptions.entries.random(),
-                    algo = listOf(signatureAlgo.oid),
-                    len = signatureAlgo.digest.outputLength.bits,
-                    curve = if (signatureAlgo.isEc) signatureAlgo.algorithm.toJwsAlgorithm()
-                        .getOrThrow().ecCurve!!.oid else null
-                ),
-                certParameters = CscCertificateParameters(
-                    status = if (isValid) CscCertificateParameters.CertStatus.VALID else CscCertificateParameters.CertStatus.entries.random(),
-                    certificates = listOf(EphemeralKeyWithSelfSignedCert().getCertificate()!!),
-                    issuerDN = uuid4().toString(),
-                    serialNumber = uuid4().toString(),
-                    subjectDN = uuid4().toString(),
-                ),
-                authParameters = CscAuthParameter(
-                    mode = CscAuthParameter.AuthMode.EXPLICIT,
-                ),
-                scal = CredentialInfo.ScalOptions.entries.random(),
-                multisign = 1U,
-                lang = "de"
-            )
-        }
-
-        fun getDocumentDigests(): List<OAuthDocumentDigest> = (1..Random.nextInt(10)).map {
-            OAuthDocumentDigest(
-                hash = uuid4().bytes.encodeToString(Base64UrlStrict).decodeToByteArray(Base64UrlStrict),
-                label = uuid4().toString(),
-            )
-        }
-    }
 
     val dummyValueProvider = DummyValueProvider()
     val rqesWalletService = RqesOpenId4VpHolder()
@@ -109,7 +60,7 @@ class RqesOpenId4VpHolderTest : FreeSpec({
         rqesWalletService.setSigningCredential(validCert)
 
         "CscAuthDetails respects SigningCredential" {
-            val digests = dummyValueProvider.getDocumentDigests()
+            val digests = dummyValueProvider.buildDocumentDigests()
             val testAuthDetails = rqesWalletService.getCscAuthenticationDetails(digests, validSigningAlgo.digest)
             with(testAuthDetails as? CscAuthorizationDetails) {
                 this shouldNotBe null
@@ -127,14 +78,14 @@ class RqesOpenId4VpHolderTest : FreeSpec({
         }
 
         "CscDocumentDigest respects SigningCredential" {
-            val digests = dummyValueProvider.getDocumentDigests()
+            val digests = dummyValueProvider.buildDocumentDigests()
             val testDocumentDigests = rqesWalletService.getCscDocumentDigests(
                 digests,
                 rqesWalletService.signingCredential!!.supportedSigningAlgorithms.first()
             )
-            with(testDocumentDigests as? CscDocumentDigest) {
+            with(testDocumentDigests) {
                 this shouldNotBe null
-                this!!.signAlgoOid shouldBe validSigningAlgo.oid
+                this.signAlgoOid shouldBe validSigningAlgo.oid
                 // These change before each test
                 this.signatureFormat shouldBe rqesWalletService.signatureProperties.signatureFormat
                 this.conformanceLevel shouldBe rqesWalletService.signatureProperties.conformanceLevel
@@ -160,7 +111,7 @@ class RqesOpenId4VpHolderTest : FreeSpec({
         }
 
         "AuthenticationRequest CREDENTIAL" {
-            val documentDigests = dummyValueProvider.getDocumentDigests()
+            val documentDigests = dummyValueProvider.buildDocumentDigests()
             val request = rqesWalletService.createCredentialAuthenticationRequest(
                 documentDigests = documentDigests,
                 redirectUrl = "someOtherURL",
@@ -191,7 +142,7 @@ class RqesOpenId4VpHolderTest : FreeSpec({
         }
 
         "TokenRequest" {
-            val documentDigests = dummyValueProvider.getDocumentDigests()
+            val documentDigests = dummyValueProvider.buildDocumentDigests()
             val request = rqesWalletService.createOAuth2TokenRequest(
                 state = uuid4().toString(),
                 authorization = OAuth2Client.AuthorizationForToken.Code(uuid4().toString()),
