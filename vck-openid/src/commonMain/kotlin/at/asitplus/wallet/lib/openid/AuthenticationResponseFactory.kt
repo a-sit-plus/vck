087a2fa1b083d77821b20c676ac98d7a52617d74
Simon Mueller <simon.mueller@a-sit.at>
2025-04-18 13:49:20 +0200

    Throw exception in case of invalid request

--- vck-openid/src/commonMain/kotlin/at/asitplus/wallet/lib/openid/AuthenticationResponseFactory.kt
+++ vck-openid/src/commonMain/kotlin/at/asitplus/wallet/lib/openid/AuthenticationResponseFactory.kt
@@ -2,6 +2,8 @@ package at.asitplus.wallet.lib.openid
 
 import at.asitplus.openid.*
 import at.asitplus.openid.OpenIdConstants.ResponseMode.*
+import at.asitplus.openid.RelyingPartyMetadata
+import at.asitplus.openid.RequestParametersFrom
 import at.asitplus.signum.indispensable.josef.JsonWebKey
 import at.asitplus.signum.indispensable.josef.JweAlgorithm
 import at.asitplus.signum.indispensable.josef.JweHeader
@@ -15,6 +17,7 @@ import at.asitplus.wallet.lib.oidvci.encodeToParameters
 import at.asitplus.wallet.lib.oidvci.formUrlEncode
 import io.github.aakira.napier.Napier
 import io.ktor.http.*
+import kotlinx.serialization.builtins.serializer
 import kotlin.coroutines.cancellation.CancellationException
 import kotlin.random.Random
 
@@ -93,7 +96,7 @@ internal class AuthenticationResponseFactory(
     }
 
     /**
-     * Per OID4VP, the response may either be signed, or encrypted, or even signed and encrypted
+     * Per OID4VP, the response must either be signed, or encrypted, or even signed and encrypted
      */
     @Throws(OAuth2Exception::class, CancellationException::class)
     private suspend fun buildJarm(
@@ -104,7 +107,7 @@ internal class AuthenticationResponseFactory(
     } else if (response.requestsSignature()) {
         sign(response.params)
     } else {
-        odcJsonSerializer.encodeToString(response)
+        throw InvalidRequest("Response must be either signed, encrypted or both.")
     }
 
     private suspend fun sign(payload: AuthenticationResponseParameters): String =
