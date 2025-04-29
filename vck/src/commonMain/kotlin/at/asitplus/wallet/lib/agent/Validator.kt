435e0d156c49da94ce95719fb23c15fdb0132132
acrusage-iaik <139230478+acrusage-iaik@users.noreply.github.com>
2025-04-29 17:56:54 +0200

    Fix: actually catch exception instead of rethrowing

--- vck/src/commonMain/kotlin/at/asitplus/wallet/lib/agent/Validator.kt
+++ vck/src/commonMain/kotlin/at/asitplus/wallet/lib/agent/Validator.kt
@@ -1,7 +1,6 @@
 package at.asitplus.wallet.lib.agent
 
 import at.asitplus.KmmResult
-import at.asitplus.KmmResult.Companion.wrap
 import at.asitplus.signum.indispensable.CryptoPublicKey
 import at.asitplus.signum.indispensable.cosef.CoseKey
 import at.asitplus.signum.indispensable.cosef.io.ByteStringWrapper
@@ -122,15 +121,15 @@ class Validator(
     /**
      * Checks the revocation state using the provided status mechanisms
      */
-    private suspend fun checkRevocationStatus(status: Status): KmmResult<TokenStatus> = runCatching {
+    private suspend fun checkRevocationStatus(status: Status): KmmResult<TokenStatus> = try {
         val resolver = tokenStatusResolver ?: {
             TokenStatus.Valid
         }
-        resolver.invoke(status)
-    }.onFailure {
+        KmmResult.success(resolver.invoke(status))
+    } catch (it: Throwable) {
         // A status mechanism is specified, but token status cannot be evaluated
-        throw TokenStatusEvaluationException(it)
-    }.wrap()
+        KmmResult.failure(TokenStatusEvaluationException(it))
+    }
 
     /**
      * Validates the content of a JWS, expected to contain a Verifiable Presentation.
@@ -429,7 +428,8 @@ class Validator(
         val isRevoked = checkRevocationStatus(sdJwt)?.let { result ->
             result.getOrNull()?.let {
                 it == TokenStatus.Invalid // TODO: is this the only status we consider "revoked"?
-            } ?: false // TODO: how to handle the case where resolving token status fails? Currently considered "not revoked"
+            }
+                ?: false // TODO: how to handle the case where resolving token status fails? Currently considered "not revoked"
         } ?: false
 
         if (isRevoked) { // How to handle "WE DO NOT KNOW"?
