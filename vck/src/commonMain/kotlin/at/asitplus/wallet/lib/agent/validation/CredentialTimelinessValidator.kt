1c93cfbd74735ee3fbf33666fcfcdd77ee769a04
acrusage-iaik <139230478+acrusage-iaik@users.noreply.github.com>
2025-05-07 15:27:47 +0200

    Refactor: Incorporate change requests

--- vck/src/commonMain/kotlin/at/asitplus/wallet/lib/agent/validation/CredentialTimelinessValidator.kt
+++ vck/src/commonMain/kotlin/at/asitplus/wallet/lib/agent/validation/CredentialTimelinessValidator.kt
@@ -15,37 +15,33 @@ data class CredentialTimelinessValidator(
         clock = clock
     ),
 ) {
-    suspend operator fun invoke(storeEntry: SubjectCredentialStore.StoreEntry): CredentialTimelinessValidationSummary {
-        val tokenStatus = when(storeEntry) {
+    suspend operator fun invoke(storeEntry: SubjectCredentialStore.StoreEntry) = CredentialTimelinessValidationSummary(
+        tokenStatus = when (storeEntry) {
             is SubjectCredentialStore.StoreEntry.Iso -> storeEntry.issuerSigned.issuerAuth.payload?.status
             is SubjectCredentialStore.StoreEntry.SdJwt -> storeEntry.sdJwt.credentialStatus
             is SubjectCredentialStore.StoreEntry.Vc -> storeEntry.vc.vc.credentialStatus
         }?.let {
             tokenStatusResolver(it)
+        },
+        timelinessValidationSummaryDetails = when (storeEntry) {
+            is SubjectCredentialStore.StoreEntry.Iso -> CredentialTimelinessValidationSummaryDetails.MdocCredential(
+                storeEntry = storeEntry,
+                isSuccess = true
+            )
+
+            is SubjectCredentialStore.StoreEntry.SdJwt -> CredentialTimelinessValidationSummaryDetails.SdJwtCredential(
+                storeEntry = storeEntry,
+                isSuccess = true
+            )
+
+            is SubjectCredentialStore.StoreEntry.Vc -> CredentialTimelinessValidationSummaryDetails.VerifiableCredential(
+                storeEntry = storeEntry,
+                verifiableCredentialJwsTimelinessValidator(vcJws = storeEntry.vc),
+            )
         }
-
-        return CredentialTimelinessValidationSummary(
-            tokenStatus = tokenStatus,
-            timelinessValidationSummaryDetails = when(storeEntry) {
-                is SubjectCredentialStore.StoreEntry.Iso -> CredentialTimelinessValidationSummaryDetails.MdocCredential(
-                    storeEntry = storeEntry,
-                    isSuccess = true
-                )
-
-                is SubjectCredentialStore.StoreEntry.SdJwt -> CredentialTimelinessValidationSummaryDetails.SdJwtCredential(
-                    storeEntry = storeEntry,
-                    isSuccess = true
-                )
-
-                is SubjectCredentialStore.StoreEntry.Vc -> CredentialTimelinessValidationSummaryDetails.VerifiableCredential(
-                    storeEntry = storeEntry,
-                    verifiableCredentialJwsTimelinessValidator(vcJws = storeEntry.vc),
-                )
-            }
-        ).also {
-            if (it.isSuccess) {
-                Napier.d("VC is timely")
-            }
+    ).also {
+        if (it.isSuccess) {
+            Napier.d("VC is timely")
         }
     }
 }
