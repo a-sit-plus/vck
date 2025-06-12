476f1e4e086baee1f45bc6162e650e2c14502a09
Michael <144923189+md4096@users.noreply.github.com>
2025-06-12 15:56:00 +0200

    Add FallbackCredential

---
+++ vck/src/commonMain/kotlin/at/asitplus/wallet/fallbackcredential/FallbackCredentialScheme.kt
@@ -0,0 +1,17 @@
+package at.asitplus.wallet.fallbackcredential
+
+import at.asitplus.wallet.lib.data.ConstantIndex
+
+data class FallbackCredentialScheme(override val vcType: String? = null, override val isoDocType: String? = null, override val sdJwtType: String? = null) : ConstantIndex.CredentialScheme {
+    companion object : ConstantIndex.CredentialScheme {
+        override val schemaUri: String = "Unknown"
+    }
+
+    override val schemaUri: String = "Unknown"
+    override val isoNamespace: String? = null
+    override val supportedRepresentations: Collection<ConstantIndex.CredentialRepresentation> = listOf(
+        ConstantIndex.CredentialRepresentation.SD_JWT,
+        ConstantIndex.CredentialRepresentation.PLAIN_JWT, ConstantIndex.CredentialRepresentation.ISO_MDOC
+    )
+    override val claimNames: Collection<String> = listOf()
+}
