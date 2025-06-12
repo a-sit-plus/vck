873a74830040e06f50994fb4b92668cbefdf1d81
Michael <144923189+md4096@users.noreply.github.com>
2025-06-12 15:56:00 +0200

    Add FallbackCredential

--- vck-openid-ktor/src/commonMain/kotlin/at/asitplus/wallet/lib/ktor/openid/OpenId4VciClient.kt
+++ vck-openid-ktor/src/commonMain/kotlin/at/asitplus/wallet/lib/ktor/openid/OpenId4VciClient.kt
@@ -17,6 +17,7 @@ import at.asitplus.wallet.lib.agent.Holder.StoreCredentialInput.*
 import at.asitplus.wallet.lib.data.AttributeIndex
 import at.asitplus.wallet.lib.data.ConstantIndex
 import at.asitplus.wallet.lib.data.vckJsonSerializer
+import at.asitplus.wallet.fallbackcredential.FallbackCredentialScheme
 import at.asitplus.wallet.lib.iso.IssuerSigned
 import at.asitplus.signum.indispensable.cosef.io.coseCompliantSerializer
 import at.asitplus.signum.indispensable.josef.JwsSigned
@@ -131,9 +132,9 @@ class OpenId4VciClient(
     }
 
     private fun SupportedCredentialFormat.resolveCredentialScheme(): ConstantIndex.CredentialScheme? =
-        (credentialDefinition?.types?.firstNotNullOfOrNull { AttributeIndex.resolveAttributeType(it) }
-            ?: sdJwtVcType?.let { AttributeIndex.resolveSdJwtAttributeType(it) }
-            ?: docType?.let { AttributeIndex.resolveIsoDoctype(it) })
+        (credentialDefinition?.types?.firstNotNullOfOrNull { AttributeIndex.resolveAttributeType(it) ?: FallbackCredentialScheme(vcType = it) }
+            ?: sdJwtVcType?.let { AttributeIndex.resolveSdJwtAttributeType(it) ?: FallbackCredentialScheme(sdJwtType = it) }
+            ?: docType?.let { AttributeIndex.resolveIsoDoctype(it)?: FallbackCredentialScheme(isoDocType = it) })
 
     /**
      * Starts the issuing process at [credentialIssuerUrl].
