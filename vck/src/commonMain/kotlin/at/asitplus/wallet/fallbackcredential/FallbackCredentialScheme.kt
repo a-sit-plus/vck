abd101b953fcfd25f728ab1ca92b253b417d69ac
Michael <144923189+md4096@users.noreply.github.com>
2025-06-27 15:09:12 +0200

    Change schemaUri

--- vck/src/commonMain/kotlin/at/asitplus/wallet/fallbackcredential/FallbackCredentialScheme.kt
+++ vck/src/commonMain/kotlin/at/asitplus/wallet/fallbackcredential/FallbackCredentialScheme.kt
@@ -4,10 +4,10 @@ import at.asitplus.wallet.lib.data.ConstantIndex
 
 data class FallbackCredentialScheme(override val vcType: String? = null, override val isoDocType: String? = null, override val sdJwtType: String? = null) : ConstantIndex.CredentialScheme {
     companion object : ConstantIndex.CredentialScheme {
-        override val schemaUri: String = "Unknown"
+        override val schemaUri: String = "https://wallet.a-sit.at/schemas/1.0.0/unknown.json"
     }
 
-    override val schemaUri: String = "Unknown"
+    override val schemaUri: String = "https://wallet.a-sit.at/schemas/1.0.0/unknown.json"
     override val isoNamespace: String? = null
     override val supportedRepresentations: Collection<ConstantIndex.CredentialRepresentation> = listOf(
         ConstantIndex.CredentialRepresentation.SD_JWT,
