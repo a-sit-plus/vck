7a220493a0d1bfbad26323b4f957a36b6a06a570
Michael <144923189+md4096@users.noreply.github.com>
2025-07-04 10:20:44 +0200

    Optimize fallback schemes

--- vck/src/commonMain/kotlin/at/asitplus/wallet/fallbackCredential/isoMdocFallbackCredentialScheme/IsoMdocFallbackCredentialScheme.kt
+++ vck/src/commonMain/kotlin/at/asitplus/wallet/fallbackCredential/isoMdocFallbackCredentialScheme/IsoMdocFallbackCredentialScheme.kt
@@ -2,7 +2,7 @@ package at.asitplus.wallet.fallbackCredential.isoMdocFallbackCredentialScheme
 
 import at.asitplus.wallet.lib.data.ConstantIndex
 
-data class IsoMdocFallbackCredentialScheme(override val isoDocType: String? = null, override val isoNamespace: String? = isoDocType) : ConstantIndex.CredentialScheme {
+data class IsoMdocFallbackCredentialScheme(override val isoDocType: String, override val isoNamespace: String = isoDocType) : ConstantIndex.CredentialScheme {
     companion object : ConstantIndex.CredentialScheme {
         override val schemaUri: String = "https://wallet.a-sit.at/schemas/1.0.0/unknown_iso.json"
     }
