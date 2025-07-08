16c36e9886490f7d350fcf74e102cf7c9d2a59af
Christian Kollmann <christian.kollmann@a-sit.at>
2025-07-08 14:01:14 +0200

    Deprecate serialize and deserialize methods in data classes (#314)

--- openid-data-classes/src/commonMain/kotlin/at/asitplus/iso/ValidityInfo.kt
+++ openid-data-classes/src/commonMain/kotlin/at/asitplus/iso/ValidityInfo.kt
@@ -1,9 +1,9 @@
 package at.asitplus.iso
 
-import kotlinx.datetime.Instant
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.cbor.ValueTags
+import kotlin.time.Instant
 
 /**
  * Part of the ISO/IEC 18013-5:2021 standard: Data structure for MSO (9.1.2.4)
