39e635533492357512db234181cea7f7fc434963
Christian Kollmann <christian.kollmann@a-sit.at>
2025-07-08 14:01:14 +0200

    Deprecate serialize and deserialize methods in data classes (#314)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,16 @@
 import org.jetbrains.dokka.gradle.DokkaMultiModuleTask
 
 plugins {
-    id("at.asitplus.gradle.vclib-conventions")
-    id("io.kotest.multiplatform") version libs.versions.kotest
+    val kotlinVer = System.getenv("KOTLIN_VERSION_ENV")?.ifBlank { null } ?: libs.versions.kotlin.get()
+    val kotestVer = System.getenv("KOTEST_VERSION_ENV")?.ifBlank { null } ?: libs.versions.kotest.get()
+    val kspVer = System.getenv("KSP_VERSION_ENV")?.ifBlank { null } ?: "$kotlinVer-${libs.versions.ksp.get()}"
+
+    id("at.asitplus.gradle.conventions") version "20250714"
+    id("io.kotest") version kotestVer
+    kotlin("multiplatform") version kotlinVer apply false
+    kotlin("plugin.serialization") version kotlinVer apply false
+    id("com.android.library") version libs.versions.agp.get() apply (false)
+    id("com.google.devtools.ksp") version kspVer
 }
 
 //access dokka plugin from conventions plugin's classpath in root project → no need to specify version
