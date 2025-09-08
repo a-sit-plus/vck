2a2f801d16ae0cce3145fcd7934f13ce0b5a0e91
Simon Mueller <simon.mueller@a-sit.at>
2025-09-08 16:54:42 +0200

    Make DCQLCredentialQuery.meta required

--- openid-data-classes/src/commonMain/kotlin/at/asitplus/openid/dcql/DCQLCredentialQuery.kt
+++ openid-data-classes/src/commonMain/kotlin/at/asitplus/openid/dcql/DCQLCredentialQuery.kt
@@ -42,8 +42,9 @@ sealed interface DCQLCredentialQuery {
      * this object are defined per Credential Format. Examples of those are in Appendix B.3.5 and
      * Appendix B.2.3. If empty, no specific constraints are placed on the metadata or validity
      * of the requested Credential.
+     *
+     * Compare: https://github.com/openid/OpenID4VP/issues/590
      */
-    //TODO: Add empty constraints?
     @SerialName(SerialNames.META)
     val meta: DCQLCredentialMetadataAndValidityConstraints
 
@@ -152,15 +153,13 @@ sealed interface DCQLCredentialQuery {
             throw IllegalArgumentException("Incompatible credential format")
         }
 
-        meta?.let {
-            Procedures.validateCredentialMetadataAndValidityConstraints(
-                credential = credential,
-                credentialFormatIdentifier = credentialFormatExtractor(credential),
-                credentialMetadataAndValidityConstraints = it,
-                mdocCredentialDoctypeExtractor = mdocCredentialDoctypeExtractor,
-                sdJwtCredentialTypeExtractor = sdJwtCredentialTypeExtractor,
-            ).getOrThrow()
-        }
+        Procedures.validateCredentialMetadataAndValidityConstraints(
+            credential = credential,
+            credentialFormatIdentifier = credentialFormatExtractor(credential),
+            credentialMetadataAndValidityConstraints = meta,
+            mdocCredentialDoctypeExtractor = mdocCredentialDoctypeExtractor,
+            sdJwtCredentialTypeExtractor = sdJwtCredentialTypeExtractor,
+        ).getOrThrow()
 
         val claimQueries = claims
             ?: return KmmResult.success(DCQLCredentialQueryMatchingResult.AllClaimsMatchingResult)
