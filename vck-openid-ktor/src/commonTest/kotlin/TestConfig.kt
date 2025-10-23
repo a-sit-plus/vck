41b5cfb2b9476af6b642a7be132050eddad687ff
Christian Kollmann <christian.kollmann@a-sit.at>
2025-10-23 12:12:35 +0200

    Rename class to match file name

--- vck-openid-ktor/src/commonTest/kotlin/TestConfig.kt
+++ vck-openid-ktor/src/commonTest/kotlin/TestConfig.kt
@@ -3,7 +3,7 @@ import de.infix.testBalloon.framework.TestSession
 import io.github.aakira.napier.DebugAntilog
 import io.github.aakira.napier.Napier
 
-class ModuleTestSession : TestSession() {
+class TestConfig : TestSession() {
     init {
         Napier.takeLogarithm()
         Napier.base(DebugAntilog())
