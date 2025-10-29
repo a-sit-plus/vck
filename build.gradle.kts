636b4fbd6d6a4b426e762b3cb7876d5e1239bad1
Bernd Prünster <bernd.pruenster@a-sit.at>
2025-10-29 14:15:26 +0100

    dependency updates and cleanups

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,51 +35,7 @@ subprojects {
         //doesn't build with latest signum, but doesn't matter either
         tasks.findByName("iosX64Test")?.let { it.enabled = false }
         tasks.findByName("linkDebugTestIosX64")?.let { it.enabled = false }
-
-        /*help the linker (yes, this is absolutely bonkers!)*/
-        if (org.gradle.internal.os.OperatingSystem.current() == org.gradle.internal.os.OperatingSystem.MAC_OS) {
-            val devDir = System.getenv("DEVELOPER_DIR")?.ifEmpty { null }.let {
-                if (it == null) {
-                    val output = ByteArrayOutputStream()
-                    project.exec {
-                        commandLine("xcode-select", "-p")
-                        standardOutput = output
-                    }
-                    output.toString().trim()
-                } else it
-            }
-
-            logger.lifecycle("  DEV DIR points to $devDir")
-
-            val swiftLib = "$devDir/Toolchains/XcodeDefault.xctoolchain/usr/lib/swift/"
-
-            extensions.getByType<KotlinMultiplatformExtension>().targets.withType<KotlinNativeTarget>()
-                .configureEach {
-                    val sub = when (konanTarget.family) {
-                        Family.IOS ->
-                            if (konanTarget.name.contains("SIMULATOR", true)) "iphonesimulator" else "iphoneos"
-
-                        Family.OSX -> "macosx"
-                        Family.TVOS ->
-                            if (konanTarget.name.contains("SIMULATOR", true)) "appletvsimulator" else "appletvos"
-
-                        Family.WATCHOS ->
-                            if (konanTarget.name.contains("SIMULATOR", true)) "watchsimulator" else "watchos"
-
-                        else -> throw StopExecutionException("Konan target ${konanTarget.name} is not recognized")
-                    }
-
-                    logger.lifecycle("  KONAN target is ${konanTarget.name} which resolves to $sub")
-                    binaries.all {
-                        linkerOpts(
-                            "-L${swiftLib}$sub",
-                            "-L/usr/lib/swift"
-                        )
-                    }
-                }
-        }
-    }
-}
+}}
 
 val artifactVersion: String by extra
 group = "at.asitplus.wallet"
