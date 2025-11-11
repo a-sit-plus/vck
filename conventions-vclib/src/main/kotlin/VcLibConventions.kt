073e35c1c8c7d17e30a7779f22337b5cb8b00f44
Bernd Prünster <bernd.pruenster@a-sit.at>
2025-11-11 17:20:09 +0100

    Revert "switch to Android Host tests"
    This reverts commit 75fb43ba2b0c2cbcf5ccd55e940279934bbf603a.

--- conventions-vclib/src/main/kotlin/VcLibConventions.kt
+++ conventions-vclib/src/main/kotlin/VcLibConventions.kt
@@ -3,6 +3,7 @@
 package at.asitplus.gradle
 
 import VcLibVersions
+import at.asitplus.gradle.at.asitplus.gradle.addTestExtensions
 import com.android.build.api.dsl.androidLibrary
 import com.android.build.api.variant.KotlinMultiplatformAndroidComponentsExtension
 import org.gradle.api.Project
@@ -10,6 +11,7 @@ import org.gradle.api.artifacts.VersionCatalog
 import org.gradle.api.artifacts.VersionCatalogsExtension
 import org.gradle.api.tasks.testing.Test
 import org.gradle.kotlin.dsl.getByType
+import org.gradle.kotlin.dsl.invoke
 import org.gradle.kotlin.dsl.withType
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
@@ -74,12 +76,6 @@ class VcLibConventions : K2Conventions() {
                 }
                 tasks.withType<Test>().configureEach {
                     maxHeapSize = "8G"
-                    useJUnitPlatform {
-                        reports {
-
-                            this.
-                            junitXml.required.set(true); html.required.set(true) }
-                    }
                 }
             }
         }
@@ -111,8 +107,20 @@ fun KotlinMultiplatformExtension.vckAndroid(minSdkOverride: Int? = null) {
         }
         compileSdk = project.androidCompileSdk
 
-        withHostTestBuilder {
+        withDeviceTestBuilder {
             sourceSetTreeName = "test"
+        }.configure {
+            instrumentationRunnerArguments["timeout_msec"] = "2400000"
+            instrumentationRunnerArguments["TESTBALLOON_REPORTING_PATH_LIMIT"] = "400"
+            managedDevices {
+                localDevices {
+                    create("pixelAVD").apply {
+                        device = "Pixel 4"
+                        apiLevel = 35
+                        systemImageSource = "aosp-atd"
+                    }
+                }
+            }
         }
         packaging {
             listOf(
@@ -133,15 +141,15 @@ fun KotlinMultiplatformExtension.vckAndroid(minSdkOverride: Int? = null) {
         }
     }
     sourceSets.whenObjectAdded {
-        if (this.name == "androidHostTest") {
+        if (this.name == "androidDeviceTest") {
             dependencies {
-                implementation("de.infix.testBalloon:testBalloon-framework-core-jvm:${project.AspVersions.testballoon}")
+                implementation("de.infix.testBalloon:testBalloon-framework-core:${project.AspVersions.testballoon}")
                 implementation("androidx.test:runner:${project.AspVersions.androidTestRunner}")
             }
         }
     }
-    sourceSets.findByName("androidHostTest")?.dependencies {
-        implementation("de.infix.testBalloon:testBalloon-framework-core-jvm:${project.AspVersions.testballoon}")
+    sourceSets.findByName("androidDeviceTest")?.dependencies {
+        implementation("de.infix.testBalloon:testBalloon-framework-core:${project.AspVersions.testballoon}")
         implementation("androidx.test:runner:${project.AspVersions.androidTestRunner}")
     }
     project.extensions.getByType<KotlinMultiplatformAndroidComponentsExtension>().apply {
