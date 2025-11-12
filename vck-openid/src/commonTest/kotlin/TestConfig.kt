5f1a38747485d863f7b1ffee99817a2c43c62169
Bernd Prünster <bernd.pruenster@a-sit.at>
2025-11-12 07:40:03 +0100

    truncante android lengths more

--- vck-openid/src/commonTest/kotlin/TestConfig.kt
+++ vck-openid/src/commonTest/kotlin/TestConfig.kt
@@ -4,12 +4,15 @@ import io.github.aakira.napier.DebugAntilog
 import io.github.aakira.napier.Napier
 
 
+expect val testNameLengths: Pair<Int,Int>
+
 class TestConfig : TestSession() {
     init {
         Napier.takeLogarithm()
         Napier.base(DebugAntilog())
         at.asitplus.wallet.eupid.Initializer.initWithVCK()
         at.asitplus.wallet.mdl.Initializer.initWithVCK()
-        FreeSpec.defaultMaxLength = 3 //work around Android test name length limit
+        FreeSpec.defaultMaxLength = testNameLengths.first //work around Android test name length limit
+        FreeSpec.defaultDisplayNameMaxLength = testNameLengths.second //work around Android test name length limit
     }
 }
--- vck/src/commonTest/kotlin/TestConfig.kt
+++ vck/src/commonTest/kotlin/TestConfig.kt
@@ -3,12 +3,13 @@ import de.infix.testBalloon.framework.core.TestSession
 import io.github.aakira.napier.DebugAntilog
 import io.github.aakira.napier.Napier
 
-
+expect val testNameLengths: Pair<Int, Int>
 
 class TestConfig : TestSession() {
     init {
         Napier.takeLogarithm()
         Napier.base(DebugAntilog())
-        FreeSpec.defaultMaxLength = 3 //work around Android test name length limit
+        FreeSpec.defaultMaxLength = testNameLengths.first //work around Android test name length limit
+        FreeSpec.defaultDisplayNameMaxLength = testNameLengths.second //work around Android test name length limit
     }
 }
