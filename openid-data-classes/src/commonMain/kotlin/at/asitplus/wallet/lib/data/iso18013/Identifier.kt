e6d6508a7a2b5bde3502efe8ab495afc5632e1b4
Simon Mueller <simon.mueller@a-sit.at>
2025-11-21 18:14:26 +0100

    Add IdentifierList and helper classes

--- openid-data-classes/src/commonMain/kotlin/at/asitplus/wallet/lib/data/iso18013/Identifier.kt
+++ openid-data-classes/src/commonMain/kotlin/at/asitplus/wallet/lib/data/iso18013/Identifier.kt
@@ -4,15 +4,21 @@ import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.builtins.ByteArraySerializer
 import kotlinx.serialization.cbor.ByteString
-import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.descriptors.buildClassSerialDescriptor
 import kotlinx.serialization.encoding.Decoder
 import kotlinx.serialization.encoding.Encoder
 
 /**
  * Identifier = bstr
  *
- * Needs to be a class containing member with ByteString annotation for correct cbor major type
- * and compatability as map key
+ * !! This only works correctly if the cbor serializer uses `alwaysUseByteString = true`
+ * !! because a custom serializer does not honor the @ByteString annotation. Currently it is only cosmetic.
+ *
+ * Things that do not work at time of writing(21.11.2025):
+ * I.   We cannot use ByteArray directly because of kotlins way of handling equality and kotlinx handling of @ByteString
+ * II.  We cannot use value class bc its jvm specific and even then equality cannot be correctly implemented bc I.
+ * III. We cannot use the default serializer of [Identifier] because you would encode the class instead of just [value]
+ * IV.  We cannot force the custom serializer to use Type 2 Major type (aka @ByteString) when encoding [value]
  */
 @Serializable(with = Identifier.Serializer::class)
 data class Identifier(@ByteString val value: ByteArray) {
@@ -30,7 +36,7 @@ data class Identifier(@ByteString val value: ByteArray) {
 
     object Serializer : KSerializer<Identifier> {
         private val delegate = ByteArraySerializer()
-        override val descriptor: SerialDescriptor = delegate.descriptor
+        override val descriptor = buildClassSerialDescriptor("at.asitplus.wallet.lib.data.iso18013.Identifier")
 
         override fun serialize(encoder: Encoder, value: Identifier) {
             encoder.encodeSerializableValue(delegate, value.value)
