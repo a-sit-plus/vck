61144c736c4c509fbba9f78cebccf257a31d85dd
Simon Mueller <simon.mueller@a-sit.at>
2025-11-26 15:05:29 +0100

    Refactor to TransformingSerializer

--- openid-data-classes/src/commonMain/kotlin/at/asitplus/wallet/lib/data/iso18013/Identifier.kt
+++ openid-data-classes/src/commonMain/kotlin/at/asitplus/wallet/lib/data/iso18013/Identifier.kt
@@ -1,26 +1,25 @@
 package at.asitplus.wallet.lib.data.iso18013
 
-import kotlinx.serialization.KSerializer
+
+import at.asitplus.signum.indispensable.cosef.io.coseCompliantSerializer
+import at.asitplus.signum.indispensable.io.TransformingSerializerTemplate
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.builtins.ByteArraySerializer
 import kotlinx.serialization.cbor.ByteString
-import kotlinx.serialization.descriptors.buildClassSerialDescriptor
-import kotlinx.serialization.encoding.Decoder
-import kotlinx.serialization.encoding.Encoder
+import kotlinx.serialization.cbor.Cbor.Default.CoseCompliant
 
 /**
  * Identifier = bstr
  *
- * !! This only works correctly if the cbor serializer uses `alwaysUseByteString = true`
- * !! because a custom serializer does not honor the @ByteString annotation. Currently it is only cosmetic.
+ * This only works correctly if the cbor serializer uses `alwaysUseByteString = true`
+ * which holds for any serializer which implements [CoseCompliant] such as [coseCompliantSerializer].
  *
- * Things that do not work at time of writing(21.11.2025):
- * I.   We cannot use ByteArray directly because of kotlins way of handling equality and kotlinx handling of @ByteString
- * II.  We cannot use value class bc its jvm specific and even then equality cannot be correctly implemented bc I.
- * III. We cannot use the default serializer of [Identifier] because you would encode the class instead of just [value]
- * IV.  We cannot force the custom serializer to use Type 2 Major type (aka @ByteString) when encoding [value]
+ * The `@ByteString` annotation is currently only cosmetic because a custom serializer cannot not honor it
+ * due to how it's wired in the backend.
+ *
+ * We cannot use ByteArray directly because of Kotlin's way of handling equality and kotlinx's handling of `@ByteString`
  */
-@Serializable(with = Identifier.Serializer::class)
+@Serializable(with = Identifier.TransformingSerializer::class)
 data class Identifier(@ByteString val value: ByteArray) {
 
     override fun equals(other: Any?): Boolean {
@@ -34,16 +33,9 @@ data class Identifier(@ByteString val value: ByteArray) {
 
     override fun hashCode(): Int = value.contentHashCode()
 
-    object Serializer : KSerializer<Identifier> {
-        private val delegate = ByteArraySerializer()
-        override val descriptor = buildClassSerialDescriptor("at.asitplus.wallet.lib.data.iso18013.Identifier")
-
-        override fun serialize(encoder: Encoder, value: Identifier) {
-            encoder.encodeSerializableValue(delegate, value.value)
-        }
-
-        override fun deserialize(decoder: Decoder): Identifier {
-            return Identifier(decoder.decodeSerializableValue(delegate))
-        }
-    }
+    object TransformingSerializer : TransformingSerializerTemplate<Identifier, ByteArray>(
+        parent = ByteArraySerializer(),
+        encodeAs = { it.value },
+        decodeAs = { Identifier(it) }
+    )
 }
