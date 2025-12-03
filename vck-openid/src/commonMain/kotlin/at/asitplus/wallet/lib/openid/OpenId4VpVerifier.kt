d8fb7eb771f901d16e486709f5f198e31e8a88f7
Gerald Palfinger <gerald.palfinger@a-sit.at>
2025-12-03 17:23:09 +0100

    Fix auto-merge issues

--- vck-openid/src/commonMain/kotlin/at/asitplus/wallet/lib/openid/OpenId4VpVerifier.kt
+++ vck-openid/src/commonMain/kotlin/at/asitplus/wallet/lib/openid/OpenId4VpVerifier.kt
@@ -127,7 +127,7 @@ class OpenId4VpVerifier(
     /** Creates challenges in authentication requests. */
     private val nonceService: NonceService = DefaultNonceService(),
     /** Used to store issued authn requests to verify the authn response to it */
-    private val idToAuthnRequestStore: MapStore<String, AuthenticationRequestParameters> = DefaultMapStore(),
+    private val stateToAuthnRequestStore: MapStore<String, AuthenticationRequestParameters> = DefaultMapStore(),
     /** Algorithm supported to decrypt responses from wallets, for [metadataWithEncryption]. */
     private val supportedJweAlgorithm: JweAlgorithm = JweAlgorithm.ECDH_ES,
     /** Algorithm supported to decrypt responses from wallets, for [metadataWithEncryption]. */
@@ -239,7 +239,7 @@ class OpenId4VpVerifier(
             is CreationOptions.Query -> {
                 require(clientIdScheme !is ClientIdScheme.CertificateSanDns) // per OpenID4VP d23 5.10.4
                 URLBuilder(creationOptions.walletUrl).apply {
-                    createAuthnRequest(requestOptions, id).encodeToParameters()
+                    createAuthnRequest(requestOptions).encodeToParameters()
                         .forEach { parameters.append(it.key, it.value) }
                 }.buildString().toCreatedRequest()
             }
@@ -255,7 +255,7 @@ class OpenId4VpVerifier(
                         .forEach { parameters.append(it.key, it.value) }
                 }.buildString().toCreatedRequest {
                     catching {
-                        vckJsonSerializer.encodeToString(createAuthnRequest(requestOptions, id, it))
+                        vckJsonSerializer.encodeToString(createAuthnRequest(requestOptions, it))
                     }
                 }
             }
@@ -265,7 +265,7 @@ class OpenId4VpVerifier(
                 URLBuilder(creationOptions.walletUrl).apply {
                     JarRequestParameters(
                         clientId = clientIdScheme.clientId,
-                        request = createAuthnRequestAsSignedRequestObject(requestOptions, id).getOrThrow().serialize(),
+                        request = createAuthnRequestAsSignedRequestObject(requestOptions).getOrThrow().serialize(),
                     ).encodeToParameters()
                         .forEach { parameters.append(it.key, it.value) }
                 }.buildString().toCreatedRequest()
@@ -283,7 +283,7 @@ class OpenId4VpVerifier(
                 }.buildString()
                     .toCreatedRequest {
                         catching {
-                            createAuthnRequestAsSignedRequestObject(requestOptions, id, it).getOrThrow().serialize()
+                            createAuthnRequestAsSignedRequestObject(requestOptions, it).getOrThrow().serialize()
                         }
                     }
             }
@@ -311,10 +311,9 @@ class OpenId4VpVerifier(
      */
     suspend fun createAuthnRequestAsSignedRequestObject(
         requestOptions: RequestOptions,
-        id: String,
         requestObjectParameters: RequestObjectParameters? = null,
     ): KmmResult<JwsSigned<AuthenticationRequestParameters>> = catching {
-        val requestObject = createAuthnRequest(requestOptions, id, requestObjectParameters)
+        val requestObject = createAuthnRequest(requestOptions, requestObjectParameters)
         val siopClientId = "https://self-issued.me/v2"
         val issuer = when (clientIdScheme) {
             is ClientIdScheme.PreRegistered -> clientIdScheme.issuerUri ?: clientIdScheme.clientId
@@ -336,7 +335,6 @@ class OpenId4VpVerifier(
      */
     suspend fun createAuthnRequest(
         requestOptions: RequestOptions,
-        id: String,
         requestObjectParameters: RequestObjectParameters? = null,
     ) = prepareAuthnRequest(
         requestOptions = requestOptions,
@@ -369,7 +367,7 @@ class OpenId4VpVerifier(
         clientMetadata = clientMetadata(),
         idTokenType = if (isSiop) IdTokenType.SUBJECT_SIGNED.text else null,
         responseMode = responseMode,
-        state = if (!isDcApi) state else null,
+        state = if (!isAnyDcApi) state else null,
         dcqlQuery = if (isDcql) toDCQLQuery() else null,
         presentationDefinition = if (isPresentationExchange)
             toPresentationDefinition(containerJwt, containerSdJwt) else null,
