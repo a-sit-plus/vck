c7d58ed5af939561095ecd56902fece6b44d8ad7
Christian Kollmann <christian.kollmann@a-sit.at>
2025-12-03 13:17:42 +0100

    OID4VP: Remove state from result classes

--- vck-openid/src/commonTest/kotlin/at/asitplus/wallet/lib/openid/RedirectUriClientTest.kt
+++ vck-openid/src/commonTest/kotlin/at/asitplus/wallet/lib/openid/RedirectUriClientTest.kt
@@ -215,7 +215,6 @@ val RedirectUriClientTest by testSuite {
             it.verifierOid4vp.validateAuthnResponse(authnResponse.url)
                 .shouldBeInstanceOf<AuthnResponseResult.Success>().apply {
                     vp.freshVerifiableCredentials.shouldNotBeEmpty()
-                    state.shouldBe(expectedState)
                 }
         }
 
