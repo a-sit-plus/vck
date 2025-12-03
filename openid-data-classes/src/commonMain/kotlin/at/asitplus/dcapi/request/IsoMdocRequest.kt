8acd29d6f954349eb9feddae86aa7507ba5e137b
Gerald Palfinger <gerald.palfinger@a-sit.at>
2025-12-03 19:43:52 +0100

    Fix serialization of ISO request

--- openid-data-classes/src/commonMain/kotlin/at/asitplus/dcapi/request/IsoMdocRequest.kt
+++ openid-data-classes/src/commonMain/kotlin/at/asitplus/dcapi/request/IsoMdocRequest.kt
@@ -1,14 +1,18 @@
 package at.asitplus.dcapi.request
 
 import at.asitplus.iso.DeviceRequest
+import at.asitplus.iso.DeviceRequestBase64UrlSerializer
 import at.asitplus.iso.EncryptionInfo
+import at.asitplus.iso.EncryptionInfoBase64UrlSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class IsoMdocRequest(
     @SerialName("deviceRequest")
-    override val deviceRequest: DeviceRequest,
+    @Serializable(with = DeviceRequestBase64UrlSerializer::class)
+    val deviceRequest: DeviceRequest,
     @SerialName("encryptionInfo")
-    override val encryptionInfo: EncryptionInfo,
-) : IsoMdocRequestInterface
+    @Serializable(with = EncryptionInfoBase64UrlSerializer::class)
+    val encryptionInfo: EncryptionInfo,
+)// : IsoMdocRequestInterface
