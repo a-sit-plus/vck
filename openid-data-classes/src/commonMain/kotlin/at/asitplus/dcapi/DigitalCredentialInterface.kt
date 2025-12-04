457fc8807bd5dad5728f1cbf293b18df90d38fd1
Gerald Palfinger <gerald.palfinger@a-sit.at>
2025-12-04 15:54:33 +0100

    Implement Annex C verification until decryption

--- openid-data-classes/src/commonMain/kotlin/at/asitplus/dcapi/DigitalCredentialInterface.kt
+++ openid-data-classes/src/commonMain/kotlin/at/asitplus/dcapi/DigitalCredentialInterface.kt
@@ -1,5 +1,7 @@
 package at.asitplus.dcapi
 
+import at.asitplus.openid.AuthenticationResponseParameters
+import at.asitplus.openid.ResponseParametersFrom
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.JsonClassDiscriminator
@@ -10,25 +12,36 @@ sealed class DigitalCredentialInterface(
 //    /** `org-iso-mdoc`. */
 //    @SerialName("protocol")
 //    val protocol: ExchangeProtocolIdentifier,
-)
+
+) {
+    abstract val origin: String
+}
 
 @Serializable
 @SerialName("org-iso-mdoc")
 data class IsoMdocResponse(
     @SerialName("data")
     val data: DCAPIResponse,
+    @SerialName("origin")
+    override val origin: String,
 ) : DigitalCredentialInterface()
 
+
+// TODO this is essentially a copy of ResponseParametersFrom.DcApi
 @Serializable
-@SerialName("oid4vp-v1-signed")
+@SerialName("openid4vp-v1-signed")
 data class Oid4VpResponseSigned(
     @SerialName("data")
-    val data: Oid4vpDCAPIResponse,
+    val data: AuthenticationResponseParameters,
+    @SerialName("origin")
+    override val origin: String,
 ) : DigitalCredentialInterface()
 
 @Serializable
-@SerialName("oid4vp-v1-unsigned")
+@SerialName("openid4vp-v1-unsigned")
 data class Oid4VpResponseUnsigned(
     @SerialName("data")
-    val data: Oid4vpDCAPIResponse,
+    val data: AuthenticationResponseParameters,
+    @SerialName("origin")
+    override val origin: String,
 ) : DigitalCredentialInterface()
