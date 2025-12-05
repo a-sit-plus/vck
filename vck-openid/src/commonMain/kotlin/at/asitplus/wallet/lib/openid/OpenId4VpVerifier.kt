396bb59c7c19a1fdcec0882965dee9718410b66c
Gerald Palfinger <gerald.palfinger@a-sit.at>
2025-12-05 19:24:20 +0100

    Return a result for Iso validation

--- vck-openid/src/commonMain/kotlin/at/asitplus/wallet/lib/openid/OpenId4VpVerifier.kt
+++ vck-openid/src/commonMain/kotlin/at/asitplus/wallet/lib/openid/OpenId4VpVerifier.kt
@@ -445,7 +445,7 @@ class OpenId4VpVerifier(
     suspend fun validateAuthnResponse(
         input: OpenId4VpResponse,
         externalId: String,
-    ): AuthnResponseResult  = catchingUnwrapped {
+    ): AuthnResponseResult = catchingUnwrapped {
         responseParser.parseAuthnResponse(input)
     }.getOrElse {
         return AuthnResponseResult.Error("Can't parse input: $input", cause = it)
