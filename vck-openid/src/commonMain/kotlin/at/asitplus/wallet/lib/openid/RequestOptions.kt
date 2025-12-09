a6c85ba1675948bae210b38dab71d1ed296ba4d1
Gerald Palfinger <gerald.palfinger@a-sit.at>
2025-12-09 13:34:03 +0100

    Add remaining deprecations

---
+++ vck-openid/src/commonMain/kotlin/at/asitplus/wallet/lib/openid/RequestOptions.kt
@@ -0,0 +1,8 @@
+package at.asitplus.wallet.lib.openid
+
+@Deprecated(
+    message = "RequestOptions has been renamed to OpenId4VpRequestOptions and moved to at.asitplus.wallet.lib.openid.OpenId4VpRequestOptions",
+    replaceWith = ReplaceWith("at.asitplus.wallet.lib.openid.OpenId4VpRequestOptions"),
+    level = DeprecationLevel.ERROR
+)
+class RequestOptions
