13a9e825db6df71e72bc12741e9b6181ba2dff7d
Gerald Palfinger <gerald.palfinger@a-sit.at>
2025-12-09 12:39:27 +0100

    Add deprecations

---
+++ vck-openid/src/commonMain/kotlin/at/asitplus/wallet/lib/oidvci/MapStore.kt
@@ -0,0 +1,8 @@
+package at.asitplus.wallet.lib.oidvci
+
+@Deprecated(
+    message = "MapStore has been moved to at.asitplus.wallet.lib.utils.MapStore",
+    replaceWith = ReplaceWith("at.asitplus.wallet.lib.utils.MapStore"),
+    level = DeprecationLevel.ERROR
+)
+class MapStore
---
+++ vck-openid/src/commonMain/kotlin/at/asitplus/wallet/lib/oidvci/NonceService.kt
@@ -0,0 +1,8 @@
+package at.asitplus.wallet.lib.oidvci
+
+@Deprecated(
+    message = "NonceService has been moved to at.asitplus.wallet.lib.NonceService",
+    replaceWith = ReplaceWith("at.asitplus.wallet.lib.NonceService"),
+    level = DeprecationLevel.ERROR
+)
+class NonceService
