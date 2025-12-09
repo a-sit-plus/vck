7a4aeba08bf8f51ad925185540439ce5f3df7b03
Gerald Palfinger <gerald.palfinger@a-sit.at>
2025-12-09 13:34:03 +0100

    Add remaining deprecations

---
+++ vck-openid/src/commonMain/kotlin/at/asitplus/wallet/lib/oidvci/DefaultNonceService.kt
@@ -0,0 +1,8 @@
+package at.asitplus.wallet.lib.oidvci
+
+@Deprecated(
+    message = "DefaultNonceService has been moved to at.asitplus.wallet.lib.DefaultNonceService",
+    replaceWith = ReplaceWith("at.asitplus.wallet.lib.DefaultNonceService"),
+    level = DeprecationLevel.ERROR
+)
+class DefaultNonceService
