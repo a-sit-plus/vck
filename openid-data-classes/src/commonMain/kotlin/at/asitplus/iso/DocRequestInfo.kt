f489cf20ae51d821d85529d6d2e901a42fab6860
Henrik Zant <henrik.zant@tugraz.at>
2026-01-08 14:42:47 +0100

    ISO: provided equals and hashcode overrides for data class

--- openid-data-classes/src/commonMain/kotlin/at/asitplus/iso/DocRequestInfo.kt
+++ openid-data-classes/src/commonMain/kotlin/at/asitplus/iso/DocRequestInfo.kt
@@ -18,4 +18,34 @@ data class DocRequestInfo(
     @SerialName("docResponseEncryption")
     val docResponseEncryption: EncryptionParameters? = null,
     // TODO: Implement alternativeDataElements
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || this::class != other::class) return false
+
+        other as DocRequestInfo
+
+        if (uniqueDocSetRequired != other.uniqueDocSetRequired) return false
+        if (issuerIdentifiers != null && other.issuerIdentifiers != null) {
+            if (issuerIdentifiers.size != other.issuerIdentifiers.size) return false
+            if (!issuerIdentifiers.zip(other.issuerIdentifiers).all {
+                (a, b) -> a.contentEquals(b)
+            }) return false
+        } else if (issuerIdentifiers != other.issuerIdentifiers) return false
+
+        if (maximumResponseSize != other.maximumResponseSize) return false
+        if (zkRequest != other.zkRequest) return false
+        if (docResponseEncryption != other.docResponseEncryption) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = uniqueDocSetRequired?.hashCode() ?: 0
+        result = 31 * result + (issuerIdentifiers?.fold(1) { acc, arr -> 31 * acc + arr.contentHashCode() } ?: 0)
+        result = 31 * result + (maximumResponseSize?.hashCode() ?: 0)
+        result = 31 * result + (zkRequest?.hashCode() ?: 0)
+        result = 31 * result + (docResponseEncryption?.hashCode() ?: 0)
+        return result
+    }
+}
