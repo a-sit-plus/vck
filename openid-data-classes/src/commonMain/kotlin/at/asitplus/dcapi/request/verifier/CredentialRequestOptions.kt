267a914218f607c21db2395d1277ef6f6f998648
Gerald Palfinger <gerald.palfinger@a-sit.at>
2026-02-06 15:31:51 +0100

    Add annotation to changed class

--- openid-data-classes/src/commonMain/kotlin/at/asitplus/dcapi/request/verifier/CredentialRequestOptions.kt
+++ openid-data-classes/src/commonMain/kotlin/at/asitplus/dcapi/request/verifier/CredentialRequestOptions.kt
@@ -5,12 +5,13 @@ import kotlinx.serialization.Serializable
 
 @ConsistentCopyVisibility
 @Serializable
-data class CredentialRequestOptions private constructor(
+data class CredentialRequestOptions @Deprecated("Default constructor has been replaced with create() method", ReplaceWith("CredentialRequestOptions.create(requests)")) private constructor(
     @SerialName("mediation")
     val mediation: String,
     @SerialName("digital")
     val digital: DigitalCredentialRequestOptions,
 ) {
+    @Suppress("DEPRECATION")
     companion object {
         fun create(requests: List<DigitalCredentialGetRequest>): CredentialRequestOptions =
             CredentialRequestOptions(
