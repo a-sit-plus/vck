f0d3f6a9abb3b7088dc4f4f409bcf003524c6af5
Christian Kollmann <christian.kollmann@a-sit.at>
2026-02-16 10:20:30 +0100

    DCQL: Query conversion should not throw

--- vck-openid/src/commonTest/kotlin/at/asitplus/wallet/lib/openid/OpenId4VpRequestOptionsTest.kt
+++ vck-openid/src/commonTest/kotlin/at/asitplus/wallet/lib/openid/OpenId4VpRequestOptionsTest.kt
@@ -2,21 +2,26 @@ package at.asitplus.wallet.lib.openid
 
 import at.asitplus.openid.OpenIdConstants
 import at.asitplus.openid.QCertCreationAcceptance
+import at.asitplus.openid.dcql.DCQLClaimsPathPointerSegment
 import at.asitplus.openid.dcql.DCQLIsoMdocClaimsQuery
 import at.asitplus.openid.dcql.DCQLIsoMdocCredentialMetadataAndValidityConstraints
+import at.asitplus.openid.dcql.DCQLIsoMdocCredentialQuery
 import at.asitplus.openid.dcql.DCQLJsonClaimsQuery
 import at.asitplus.openid.dcql.DCQLSdJwtCredentialMetadataAndValidityConstraints
+import at.asitplus.openid.dcql.DCQLSdJwtCredentialQuery
 import at.asitplus.signum.indispensable.asn1.ObjectIdentifier
 import at.asitplus.wallet.lib.RequestOptionsCredential
 import at.asitplus.wallet.lib.data.ConstantIndex
 import de.infix.testBalloon.framework.core.testSuite
 import io.kotest.assertions.throwables.shouldThrowAny
+import io.kotest.matchers.collections.shouldBeSingleton
 import io.kotest.matchers.collections.shouldContain
+import io.kotest.matchers.nulls.shouldBeNull
 import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeInstanceOf
 
-@Suppress("unused")
+
 val OpenId4VpRequestOptionsTest by testSuite {
 
     test("transaction data requires matching credential ids") {
@@ -37,6 +42,30 @@ val OpenId4VpRequestOptionsTest by testSuite {
                 transactionData = listOf(transactionData)
             )
         }
+    }
+
+    test("invalid credential scheme for SD-JWT should not throw when creating query") {
+        val credential = RequestOptionsCredential(
+            credentialScheme = object : ConstantIndex.CredentialScheme {
+                override val schemaUri: String = "https://example.com"
+            },
+            representation = ConstantIndex.CredentialRepresentation.SD_JWT
+        )
+        OpenId4VpRequestOptions(credentials = setOf(credential))
+            .toDCQLQuery()
+            .shouldBeNull()
+    }
+
+    test("invalid credential scheme for ISO should not throw when creating query") {
+        val credential = RequestOptionsCredential(
+            credentialScheme = object : ConstantIndex.CredentialScheme {
+                override val schemaUri: String = "https://example.com"
+            },
+            representation = ConstantIndex.CredentialRepresentation.ISO_MDOC
+        )
+        OpenId4VpRequestOptions(credentials = setOf(credential))
+            .toDCQLQuery()
+            .shouldBeNull()
     }
 
     test("dc api requires dcql and expected origins") {
@@ -83,20 +112,22 @@ val OpenId4VpRequestOptionsTest by testSuite {
             presentationMechanism = PresentationMechanismEnum.DCQL
         )
 
-        val query = options.toDCQLQuery().shouldNotBeNull()
-        val credentialQuery = query.credentials.first()
-            .shouldBeInstanceOf<at.asitplus.openid.dcql.DCQLSdJwtCredentialQuery>()
+        val credentialQuery = options.toDCQLQuery().shouldNotBeNull()
+            .credentials.shouldBeSingleton().first()
+            .shouldBeInstanceOf<DCQLSdJwtCredentialQuery>()
 
         credentialQuery.meta.shouldBeInstanceOf<DCQLSdJwtCredentialMetadataAndValidityConstraints>()
             .vctValues shouldContain ConstantIndex.AtomicAttribute2023.sdJwtType
 
-        val claims = credentialQuery.claims.shouldNotBeNull().toList()
-        claims.size shouldBe 2
+        val claims = credentialQuery.claims.shouldNotBeNull().toList().apply {
+            size shouldBe 2
+        }
         val claimNames = claims.map {
             it.shouldBeInstanceOf<DCQLJsonClaimsQuery>().path.segments.first()
-                .shouldBeInstanceOf<at.asitplus.openid.dcql.DCQLClaimsPathPointerSegment.NameSegment>()
+                .shouldBeInstanceOf<DCQLClaimsPathPointerSegment.NameSegment>()
                 .name
         }.toSet()
+
         claimNames shouldBe setOf(
             ConstantIndex.AtomicAttribute2023.CLAIM_GIVEN_NAME,
             ConstantIndex.AtomicAttribute2023.CLAIM_FAMILY_NAME
@@ -116,14 +147,14 @@ val OpenId4VpRequestOptionsTest by testSuite {
             presentationMechanism = PresentationMechanismEnum.DCQL
         )
 
-        val query = options.toDCQLQuery().shouldNotBeNull()
-        val credentialQuery = query.credentials.first()
-            .shouldBeInstanceOf<at.asitplus.openid.dcql.DCQLIsoMdocCredentialQuery>()
+        val credentialQuery = options.toDCQLQuery().shouldNotBeNull()
+            .credentials.shouldBeSingleton().first()
+            .shouldBeInstanceOf<DCQLIsoMdocCredentialQuery>()
 
         credentialQuery.meta.shouldBeInstanceOf<DCQLIsoMdocCredentialMetadataAndValidityConstraints>()
             .doctypeValue shouldBe ConstantIndex.AtomicAttribute2023.isoDocType
 
-        val claim = credentialQuery.claims.shouldNotBeNull().first()
+        val claim = credentialQuery.claims.shouldNotBeNull().shouldBeSingleton().first()
             .shouldBeInstanceOf<DCQLIsoMdocClaimsQuery>()
         claim.namespace shouldBe ConstantIndex.AtomicAttribute2023.isoNamespace
         claim.claimName shouldBe ConstantIndex.AtomicAttribute2023.CLAIM_GIVEN_NAME
