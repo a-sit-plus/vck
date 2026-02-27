15fbb207a0484b5d6b44b4ec28339bef7bd6909d
acrusage <139230478+acrusage-iaik@users.noreply.github.com>
2026-02-27 15:20:21 +0100

    Fix: Merge changes

--- vck/src/commonTest/kotlin/at/asitplus/wallet/lib/agent/AgentIsoMdocTest.kt
+++ vck/src/commonTest/kotlin/at/asitplus/wallet/lib/agent/AgentIsoMdocTest.kt
@@ -175,7 +175,7 @@ val AgentIsoMdocTest by testSuite {
                         ),
                     )
 
-                    mismatchedVerifier.verifyPresentationIsoMdoc(vp.deviceResponse, documentVerifier())
+                    mismatchedVerifier.verifyPresentationIsoMdoc(vp.deviceResponse, documentVerifier()).getOrThrow()
                         .shouldBeInstanceOf<Verifier.VerifyPresentationResult.SuccessIso>()
                         .documents.shouldBeSingleton().first().freshnessSummary.tokenStatusValidationResult
                         .shouldBeInstanceOf<TokenStatusValidationResult.Rejected>()
@@ -281,13 +281,14 @@ private fun statusListResolver(statusListIssuer: StatusListAgent) = TokenStatusR
     }
 )
 
-private suspend fun IsoMdocFixture.createPresexDeviceResponse(vararg attributeNames: String) = createPresexDeviceResponse(
-    holder = holder,
-    challenge = challenge,
-    verifierId = verifierId,
-    signer = signer,
-    attributeNames = attributeNames,
-)
+private suspend fun IsoMdocFixture.createPresexDeviceResponse(vararg attributeNames: String) =
+    createPresexDeviceResponse(
+        holder = holder,
+        challenge = challenge,
+        verifierId = verifierId,
+        signer = signer,
+        attributeNames = attributeNames,
+    )
 
 private suspend fun IsoMdocFixture.createDcqlDeviceResponse(vararg attributeNames: String) = createDcqlDeviceResponse(
     holder = holder,
