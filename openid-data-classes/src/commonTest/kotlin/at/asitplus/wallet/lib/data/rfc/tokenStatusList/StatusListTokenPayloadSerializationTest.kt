0bd478390d6e190bd8e9e87f77f26633b3a7270e
Simon Mueller <simon.mueller@a-sit.at>
2026-03-19 13:06:18 +0100

    Add regression test

---
+++ openid-data-classes/src/commonTest/kotlin/at/asitplus/wallet/lib/data/rfc/tokenStatusList/StatusListTokenPayloadSerializationTest.kt
@@ -0,0 +1,92 @@
+package at.asitplus.wallet.lib.data.rfc.tokenStatusList
+
+import at.asitplus.signum.indispensable.cosef.io.coseCompliantSerializer
+import at.asitplus.testballoon.invoke
+import at.asitplus.wallet.lib.data.vckJsonSerializer
+import at.asitplus.wallet.lib.data.rfc.tokenStatusList.cwt.claims.CwtIdentifierListClaim
+import at.asitplus.wallet.lib.data.rfc.tokenStatusList.iso18013.Identifier
+import at.asitplus.wallet.lib.data.rfc.tokenStatusList.iso18013.IdentifierInfo
+import at.asitplus.wallet.lib.data.rfc.tokenStatusList.primitives.TokenStatusBitSize
+import at.asitplus.wallet.lib.data.rfc3986.UniformResourceIdentifier
+import de.infix.testBalloon.framework.core.testSuite
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.matchers.shouldBe
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.jsonObject
+import kotlin.time.Instant
+
+private val subject = UniformResourceIdentifier("https://example.com/statuslists/1")
+private val issuedAt = Instant.fromEpochSeconds(1_700_000_000)
+private val identifierList = IdentifierList(
+    identifiers = mapOf(
+        Identifier(byteArrayOf(0x01, 0x02)) to IdentifierInfo()
+    ),
+    aggregationUri = "https://example.com/identifierlists/aggregation",
+)
+
+private val identifierListPayload = StatusListTokenPayload(
+    subject = subject,
+    issuedAt = issuedAt,
+    revocationList = identifierList,
+)
+
+private val statusListPayload = StatusListTokenPayload(
+    subject = subject,
+    issuedAt = issuedAt,
+    revocationList = StatusList(
+        compressed = byteArrayOf(0x01, 0x02, 0x03),
+        statusBitSize = TokenStatusBitSize.ONE,
+    ),
+)
+
+val StatusListTokenPayloadSerializationTest by testSuite {
+    "JSON serialization rejects identifier lists" {
+        val exception = shouldThrow<SerializationException> {
+            vckJsonSerializer.encodeToString(StatusListTokenPayload.serializer(), identifierListPayload)
+        }
+        exception.message shouldBe "IdentifierList is only supported in CBOR"
+    }
+
+    "JSON deserialization rejects identifier lists" {
+        val json = """
+            {
+              "sub": "https://example.com/statuslists/1",
+              "iat": 1700000000,
+              "identifier_list": {}
+            }
+        """.trimIndent()
+
+        val exception = shouldThrow<SerializationException> {
+            vckJsonSerializer.decodeFromString<StatusListTokenPayload>(json)
+        }
+        exception.message shouldBe "IdentifierList is only supported in CBOR"
+    }
+
+    "JSON deserialization rejects identifier_list even when status_list is present" {
+        val validStatusListJson = vckJsonSerializer
+            .encodeToJsonElement(StatusListTokenPayload.serializer(), statusListPayload)
+            .jsonObject
+        val invalidJson = JsonObject(
+            validStatusListJson + (CwtIdentifierListClaim.Specification.CLAIM_NAME to JsonObject(emptyMap()))
+        )
+
+        val exception = shouldThrow<SerializationException> {
+            vckJsonSerializer.decodeFromString<StatusListTokenPayload>(invalidJson.toString())
+        }
+        exception.message shouldBe "IdentifierList is only supported in CBOR"
+    }
+
+    "CBOR serialization still supports identifier lists" {
+        val serialized = coseCompliantSerializer.encodeToByteArray(
+            StatusListTokenPayload.serializer(),
+            identifierListPayload,
+        )
+        val deserialized = coseCompliantSerializer.decodeFromByteArray(
+            StatusListTokenPayload.serializer(),
+            serialized,
+        )
+
+        deserialized shouldBe identifierListPayload
+    }
+}
