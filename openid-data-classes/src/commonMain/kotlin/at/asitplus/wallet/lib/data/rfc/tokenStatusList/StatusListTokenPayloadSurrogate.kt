8d16aab8ed265b4fe8cf4c480e3cd0e265947732
Simon Mueller <simon.mueller@a-sit.at>
2026-03-19 13:05:59 +0100

    Update StatsListTokenPayloadSurrogate serialization

--- openid-data-classes/src/commonMain/kotlin/at/asitplus/wallet/lib/data/rfc/tokenStatusList/StatusListTokenPayloadSurrogate.kt
+++ openid-data-classes/src/commonMain/kotlin/at/asitplus/wallet/lib/data/rfc/tokenStatusList/StatusListTokenPayloadSurrogate.kt
@@ -1,19 +1,24 @@
 package at.asitplus.wallet.lib.data.rfc.tokenStatusList
 
-import at.asitplus.signum.indispensable.io.TransformingSerializerTemplate
 import at.asitplus.signum.indispensable.josef.io.InstantLongSerializer
 import at.asitplus.wallet.lib.data.rfc.tokenStatusList.cwt.claims.CwtIdentifierListClaim
 import at.asitplus.wallet.lib.data.rfc.tokenStatusList.cwt.claims.CwtStatusListClaim
 import at.asitplus.wallet.lib.data.rfc.tokenStatusList.cwt.claims.CwtTimeToLiveClaim
 import at.asitplus.wallet.lib.data.rfc.tokenStatusList.primitives.PositiveDuration
-import at.asitplus.wallet.lib.data.rfc.tokenStatusList.primitives.PositiveDurationSecondsULongSerializer
+import at.asitplus.wallet.lib.data.rfc.tokenStatusList.primitives.PositiveDurationFormatSerializer
 import at.asitplus.wallet.lib.data.rfc3986.UniformResourceIdentifier
 import at.asitplus.wallet.lib.data.rfc8392.cwt.claims.CwtExpirationTimeClaim
 import at.asitplus.wallet.lib.data.rfc8392.cwt.claims.CwtIssuedAtClaim
 import at.asitplus.wallet.lib.data.rfc8392.cwt.claims.CwtSubjectClaim
+import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.SerializationException
+import kotlinx.serialization.cbor.CborDecoder
+import kotlinx.serialization.cbor.CborEncoder
 import kotlinx.serialization.cbor.CborLabel
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import kotlin.time.Instant
 
 /**
@@ -35,13 +40,14 @@ data class StatusListTokenPayloadSurrogate(
     val expirationTime: Instant? = null,
     @SerialName("ttl")
     @CborLabel(CwtTimeToLiveClaim.Specification.CLAIM_KEY)
-    @Serializable(with = PositiveDurationSecondsULongSerializer::class)
+    @Serializable(with = PositiveDurationFormatSerializer::class)
     val timeToLive: PositiveDuration? = null,
     @SerialName("status_list")
     @CborLabel(CwtStatusListClaim.Specification.CLAIM_KEY)
     val statusList: StatusList? = null,
     @SerialName("identifier_list")
     @CborLabel(CwtIdentifierListClaim.Specification.CLAIM_KEY)
+    @Serializable(with = CborOnlyIdentifierListSerializer::class)
     val identifierList: IdentifierList? = null,
 ) {
     constructor(statusListTokenPayload: StatusListTokenPayload) : this(
@@ -54,7 +60,7 @@ data class StatusListTokenPayloadSurrogate(
     )
 
     init {
-        require(statusList == null || identifierList == null) {"Either StatusList or IdentifierList must be present"}
+        require(statusList == null || identifierList == null) { "Either StatusList or IdentifierList must be present" }
     }
 
     fun toStatusListTokenPayload() = StatusListTokenPayload(
@@ -62,18 +68,26 @@ data class StatusListTokenPayloadSurrogate(
         issuedAt = issuedAt,
         expirationTime = expirationTime,
         timeToLive = timeToLive,
-        revocationList = statusList ?: identifierList ?: throw Exception("Either StatusList or IdentifierList must be present"),
+        revocationList = statusList ?: identifierList
+        ?: throw Exception("Either StatusList or IdentifierList must be present"),
     )
 
+    internal object CborOnlyIdentifierListSerializer : KSerializer<IdentifierList> {
+        private val delegate = IdentifierList.Companion.serializer()
+        override val descriptor = delegate.descriptor
 
-    object StatusListTokenSurrogateSerializer :
-        TransformingSerializerTemplate<StatusListTokenPayload, StatusListTokenPayloadSurrogate>(
-            parent = StatusListTokenPayloadSurrogate.serializer(),
-            encodeAs = {
-                StatusListTokenPayloadSurrogate(it)
-            },
-            decodeAs = {
-                it.toStatusListTokenPayload()
-            },
-        )
+        override fun serialize(encoder: Encoder, value: IdentifierList) {
+            if (encoder !is CborEncoder) {
+                throw SerializationException("IdentifierList is only supported in CBOR")
+            }
+            delegate.serialize(encoder, value)
+        }
+
+        override fun deserialize(decoder: Decoder): IdentifierList {
+            if (decoder !is CborDecoder) {
+                throw SerializationException("IdentifierList is only supported in CBOR")
+            }
+            return delegate.deserialize(decoder)
+        }
+    }
 }
