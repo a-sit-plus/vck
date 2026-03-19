83bd0b4c9d0a51216360b43b62abcbd8d374fbe4
Simon Mueller <simon.mueller@a-sit.at>
2026-03-19 13:49:39 +0100

    Add companion objects

--- openid-data-classes/src/commonMain/kotlin/at/asitplus/wallet/lib/data/rfc/tokenStatusList/StatusListTokenPayloadSurrogate.kt
+++ openid-data-classes/src/commonMain/kotlin/at/asitplus/wallet/lib/data/rfc/tokenStatusList/StatusListTokenPayloadSurrogate.kt
@@ -1,15 +1,9 @@
 package at.asitplus.wallet.lib.data.rfc.tokenStatusList
 
 import at.asitplus.signum.indispensable.josef.io.InstantLongSerializer
-import at.asitplus.wallet.lib.data.rfc.tokenStatusList.cwt.claims.CwtIdentifierListClaim
-import at.asitplus.wallet.lib.data.rfc.tokenStatusList.cwt.claims.CwtStatusListClaim
-import at.asitplus.wallet.lib.data.rfc.tokenStatusList.cwt.claims.CwtTimeToLiveClaim
 import at.asitplus.wallet.lib.data.rfc.tokenStatusList.primitives.PositiveDuration
 import at.asitplus.wallet.lib.data.rfc.tokenStatusList.primitives.PositiveDurationFormatSerializer
 import at.asitplus.wallet.lib.data.rfc3986.UniformResourceIdentifier
-import at.asitplus.wallet.lib.data.rfc8392.cwt.claims.CwtExpirationTimeClaim
-import at.asitplus.wallet.lib.data.rfc8392.cwt.claims.CwtIssuedAtClaim
-import at.asitplus.wallet.lib.data.rfc8392.cwt.claims.CwtSubjectClaim
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
@@ -27,26 +21,47 @@ import kotlin.time.Instant
  */
 @Serializable
 data class StatusListTokenPayloadSurrogate(
-    @SerialName("sub")
-    @CborLabel(CwtSubjectClaim.Specification.CLAIM_KEY)
+    @SerialName(SerialNames.SUBJECT)
+    @CborLabel(CborLabels.SUBJECT)
     val subject: UniformResourceIdentifier,
-    @SerialName("iat")
-    @CborLabel(CwtIssuedAtClaim.Specification.CLAIM_KEY)
+    @SerialName(SerialNames.ISSUED_AT)
+    @CborLabel(CborLabels.ISSUED_AT)
     @Serializable(with = InstantLongSerializer::class)
     val issuedAt: Instant,
-    @SerialName("exp")
-    @CborLabel(CwtExpirationTimeClaim.Specification.CLAIM_KEY)
+    @SerialName(SerialNames.EXPIRATION_TIME)
+    @CborLabel(CborLabels.EXPIRATION_TIME)
     @Serializable(with = InstantLongSerializer::class)
     val expirationTime: Instant? = null,
-    @SerialName("ttl")
-    @CborLabel(CwtTimeToLiveClaim.Specification.CLAIM_KEY)
+
+    /**
+     * JSON: JsonNumber
+     * CBOR: Unsigned integer (Major Type 0)
+     */
+    @SerialName(SerialNames.TIME_TO_LIVE)
+    @CborLabel(CborLabels.TIME_TO_LIVE)
     @Serializable(with = PositiveDurationFormatSerializer::class)
     val timeToLive: PositiveDuration? = null,
-    @SerialName("status_list")
-    @CborLabel(CwtStatusListClaim.Specification.CLAIM_KEY)
+
+    /**
+     * specification: https://www.ietf.org/archive/id/draft-ietf-oauth-status-list-05.html
+     *
+     * REQUIRED*. MUST NOT be present if [identifierList] is present
+     * The status_list (status list) claim MUST specify the Status List
+     * conforming to the rules outlined in Section 4.1.
+     */
+    @SerialName(SerialNames.STATUS_LIST)
+    @CborLabel(CborLabels.STATUS_LIST)
     val statusList: StatusList? = null,
-    @SerialName("identifier_list")
-    @CborLabel(CwtIdentifierListClaim.Specification.CLAIM_KEY)
+
+    /**
+     * specification: ISO18013-5
+     * Conforming to the rules defined in 12.3.6
+     * REQUIRED*. MUST NOT be present if [statusList] is present
+     *
+     * Only defined for CBOR!
+     */
+    @SerialName(SerialNames.IDENTIFIER_LIST)
+    @CborLabel(CborLabels.IDENTIFIER_LIST)
     @Serializable(with = CborOnlyIdentifierListSerializer::class)
     val identifierList: IdentifierList? = null,
 ) {
@@ -89,5 +104,23 @@ data class StatusListTokenPayloadSurrogate(
             }
             return delegate.deserialize(decoder)
         }
+    }
+
+    data object CborLabels {
+        const val SUBJECT = 2L
+        const val ISSUED_AT = 6L
+        const val EXPIRATION_TIME = 4L
+        const val TIME_TO_LIVE = 65534L
+        const val STATUS_LIST = 65533L
+        const val IDENTIFIER_LIST = 65530L
+    }
+
+    data object SerialNames {
+        const val SUBJECT = "sub"
+        const val ISSUED_AT = "iat"
+        const val EXPIRATION_TIME = "exp"
+        const val TIME_TO_LIVE = "ttl"
+        const val STATUS_LIST = "status_list"
+        const val IDENTIFIER_LIST = "identifier_list"
     }
 }
