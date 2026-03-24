a226f1e7f8ade4a7678b0dd946d38c3ea7795cfd
Simon Mueller <simon.mueller@a-sit.at>
2026-03-24 16:37:11 +0100

    Update SdJwtSigned

--- openid-data-classes/src/commonMain/kotlin/at/asitplus/wallet/lib/jws/SdJwtSigned.kt
+++ openid-data-classes/src/commonMain/kotlin/at/asitplus/wallet/lib/jws/SdJwtSigned.kt
@@ -2,13 +2,10 @@ package at.asitplus.wallet.lib.jws
 
 import at.asitplus.KmmResult
 import at.asitplus.catching
-import at.asitplus.signum.indispensable.josef.JwsSigned
-import at.asitplus.signum.indispensable.josef.io.joseCompliantSerializer
+import at.asitplus.openid.JwsCompactTyped
+import at.asitplus.signum.indispensable.josef.JwsCompact
 import at.asitplus.wallet.lib.data.KeyBindingJws
-import at.asitplus.wallet.lib.data.VerifiableCredentialSdJwt
 import kotlinx.serialization.json.JsonElement
-import kotlinx.serialization.json.JsonObject
-import kotlinx.serialization.json.decodeFromJsonElement
 
 /**
  * Representation of a signed SD-JWT,
@@ -18,9 +15,13 @@ import kotlinx.serialization.json.decodeFromJsonElement
  * possibly ending with a [keyBindingJws], that is a JWS with payload [at.asitplus.wallet.lib.data.KeyBindingJws].
  */
 data class SdJwtSigned(
-    val jws: JwsSigned<JsonElement>,
+    /**
+     * Holds signed JWS in compact representation. To access the payload use
+     * [at.asitplus.signum.indispensable.josef.JWS.getPayload]
+     */
+    val jws: JwsCompact,
     val rawDisclosures: List<String>,
-    val keyBindingJws: JwsSigned<KeyBindingJws>? = null,
+    val keyBindingJws: JwsCompactTyped<KeyBindingJws>? = null,
     val hashInput: String,
 ) {
     override fun equals(other: Any?): Boolean {
@@ -45,69 +46,55 @@ data class SdJwtSigned(
         return result
     }
 
-    fun getPayloadAsVerifiableCredentialSdJwt(): KmmResult<VerifiableCredentialSdJwt> =
-        catching { joseCompliantSerializer.decodeFromJsonElement<VerifiableCredentialSdJwt>(jws.payload) }
-
-    fun getPayloadAsJsonObject(): KmmResult<JsonObject> =
-        catching { jws.payload as JsonObject }
-
     /**
      * Compact serialization: JWT in JWS compact serialization (Base64-URL with dots),
      * disclosures and key binding appended, separated by a tilde.
      */
     fun serialize() = keyBindingJws?.let {
         serializePresentation(jws, rawDisclosures.toSet(), it)
-    } ?: (listOf(jws.serialize()) + rawDisclosures).joinToString("~", postfix = "~")
+    } ?: (listOf(jws.toString()) + rawDisclosures).joinToString("~", postfix = "~")
 
     override fun toString(): String {
-        return "SdJwtSigned(jws=${jws.serialize()}, " +
+        return "SdJwtSigned(jws=$jws, " +
                 "rawDisclosures=$rawDisclosures, " +
-                "keyBindingJws=${keyBindingJws?.serialize()}, " +
+                "keyBindingJws=$keyBindingJws, " +
                 "hashInput='$hashInput')"
     }
 
     companion object {
         fun issued(
-            jws: JwsSigned<JsonElement>,
+            jws: JwsCompact,
             disclosures: List<String>,
         ) = SdJwtSigned(
             jws = jws,
             rawDisclosures = disclosures,
             keyBindingJws = null,
-            hashInput = (listOf(jws.serialize()) + disclosures).joinToString("~", postfix = "~")
+            hashInput = (listOf(jws.toString()) + disclosures).joinToString("~", postfix = "~")
         )
 
         fun presented(
-            jws: JwsSigned<JsonElement>,
+            jws: JwsCompact,
             disclosures: Set<String>,
-            keyBinding: JwsSigned<KeyBindingJws>,
+            keyBinding: JwsCompactTyped<KeyBindingJws>,
         ) = SdJwtSigned(
             jws = jws,
             rawDisclosures = disclosures.toList(),
             keyBindingJws = keyBinding,
-            hashInput = (listOf(jws.serialize()) + disclosures).joinToString("~", postfix = "~")
+            hashInput = (listOf(jws.toString()) + disclosures).joinToString("~", postfix = "~")
         )
 
         fun parseCatching(input: String): KmmResult<SdJwtSigned> = catching {
             require(input.contains("~")) { "Could not parse SD-JWT: $input" }
             val stringList = input.replace("[^A-Za-z0-9-_.~]".toRegex(), "").split("~")
             require(stringList.isNotEmpty()) { "Could not parse SD-JWT: $input" }
-            val jws = JwsSigned.deserialize<JsonElement>(
-                deserializationStrategy = JsonElement.serializer(),
-                it = stringList.first(),
-                json = joseCompliantSerializer
-            ).getOrThrow()
+            val jws = JwsCompact(stringList.first())
             val stringListWithoutJws = stringList.drop(1)
             val rawDisclosures = stringListWithoutJws
                 .filterNot { it.contains(".") }
                 .filterNot { it.isEmpty() }
             val keyBindingString = stringList.drop(1 + rawDisclosures.size).firstOrNull()
             val keyBindingJws = keyBindingString?.takeIf { it.isNotEmpty() }?.let {
-                JwsSigned.deserialize<KeyBindingJws>(
-                    deserializationStrategy = KeyBindingJws.serializer(),
-                    it = it,
-                    json = joseCompliantSerializer
-                ).getOrThrow()
+                JwsCompactTyped<KeyBindingJws>(it)
             }
             val hashInput = input.substringBeforeLast("~") + "~"
             SdJwtSigned(jws, rawDisclosures, keyBindingJws, hashInput)
@@ -118,10 +105,10 @@ data class SdJwtSigned(
          * disclosures and key binding appended, separated by a tilde.
          */
         fun serializePresentation(
-            jwsFromIssuer: JwsSigned<*>,
+            jwsFromIssuer: JwsCompact,
             filteredDisclosures: Set<String>,
-            keyBinding: JwsSigned<KeyBindingJws>,
-        ) = (listOf(jwsFromIssuer.serialize()) + filteredDisclosures + keyBinding.serialize()).joinToString("~")
+            keyBinding: JwsCompactTyped<KeyBindingJws>,
+        ) = (listOf(jwsFromIssuer.toString()) + filteredDisclosures + keyBinding.toString()).joinToString("~")
 
     }
 
