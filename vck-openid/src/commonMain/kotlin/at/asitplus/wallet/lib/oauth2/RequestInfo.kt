1e5c9596b127cd8b89f2e489716dd0f3aef9a1b4
Simon Mueller <simon.mueller@a-sit.at>
2026-03-24 18:39:05 +0100

    Add notes to RequestInfo

--- vck-openid/src/commonMain/kotlin/at/asitplus/wallet/lib/oauth2/RequestInfo.kt
+++ vck-openid/src/commonMain/kotlin/at/asitplus/wallet/lib/oauth2/RequestInfo.kt
@@ -1,17 +1,25 @@
 package at.asitplus.wallet.lib.oauth2
 
-import io.ktor.http.HttpMethod
+import io.ktor.http.*
 
+//TODO? use [JwsCompactTyped<JsonWebToken>] directly for dpop, clientAttestation, clientAttestationPop?
 /** Holds information about the HTTP request the client has made, to validate client authentication. */
 data class RequestInfo(
     /** URL that has been used to send this request. */
     val url: String,
     /** HTTP method that the client has used. */
     val method: HttpMethod,
-    /** Value of the header `DPoP` (RFC 9449). */
+
+    /** Value of the header `DPoP` (RFC 9449). The value of the header is a JSON Web Token (JWT) */
     val dpop: String? = null,
-    /** Value of the header `OAuth-Client-Attestation` (OAuth 2.0 Attestation-Based Client Authentication). */
+    /**
+     * Value of the header `OAuth-Client-Attestation` (OAuth 2.0 Attestation-Based Client Authentication).
+     * A JWT that conforms to the structure and syntax as defined in Section 4.2
+     */
     val clientAttestation: String? = null,
-    /** Value of the header `OAuth-Client-Attestation-PoP` (OAuth 2.0 Attestation-Based Client Authentication). */
+    /**
+     * Value of the header `OAuth-Client-Attestation-PoP` (OAuth 2.0 Attestation-Based Client Authentication).
+     * A JWT that adheres to the structure and syntax as defined in Section 4.3
+     */
     val clientAttestationPop: String? = null,
 )
