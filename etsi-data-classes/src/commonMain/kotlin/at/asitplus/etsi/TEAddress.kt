863ce881f6e2b6958c92e43362892d1bf20d490b
acrusage <139230478+acrusage-iaik@users.noreply.github.com>
2026-04-27 20:36:40 +0200

    Change: Support TEElectronicAddress.kt without website despite deviation from spec

--- etsi-data-classes/src/commonMain/kotlin/at/asitplus/etsi/TEAddress.kt
+++ etsi-data-classes/src/commonMain/kotlin/at/asitplus/etsi/TEAddress.kt
@@ -8,7 +8,7 @@ data class TEAddress(
     @SerialName(SerialNames.TE_POSTAL_ADDRESS)
     val tePostalAddress: PostalAddresses,
     @SerialName(SerialNames.TE_ELECTRONIC_ADDRESS)
-    val teElectronicAddress: ElectronicAddress,
+    val teElectronicAddress: TEElectronicAddress,
 ) {
     object SerialNames {
         const val TE_POSTAL_ADDRESS = "TEPostalAddress"
---
+++ etsi-data-classes/src/commonMain/kotlin/at/asitplus/etsi/TEElectronicAddress.kt
@@ -0,0 +1,28 @@
+package at.asitplus.etsi
+
+import kotlinx.serialization.Serializable
+import kotlin.jvm.JvmInline
+
+@Serializable
+@JvmInline
+value class TEElectronicAddress(
+    private val list: List<MultilingualPointer>
+) : List<MultilingualPointer> by list {
+    init {
+        require(list.any {
+            it.uniformResourceIdentifier.string.startsWith("mailto:")
+        }) {
+            "Expected list to contain at least 1 e-mail address identified using the scheme `malito`, but got $list."
+        }
+        /**
+         * TODO: this validation is part of the specification, but some entries in the actual LoTEs don't provide a website
+         */
+//        require(list.any {
+//            it.uniformResourceIdentifier.string.startsWith("https:")
+//        }) {
+//            "Expected list to contain at least 1 web-site, but got $list."
+//        }
+    }
+
+    constructor(vararg elements: MultilingualPointer): this(elements.toList())
+}
