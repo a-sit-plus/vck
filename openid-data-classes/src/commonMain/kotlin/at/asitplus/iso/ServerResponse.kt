cd2131b8a4498ce4dadef1543db1a3598dab3be7
Simon Mueller <simon.mueller@a-sit.at>
2026-05-20 14:06:49 +0200

    Deprecate instead of refactor

--- openid-data-classes/src/commonMain/kotlin/at/asitplus/iso/ServerResponse.kt
+++ openid-data-classes/src/commonMain/kotlin/at/asitplus/iso/ServerResponse.kt
@@ -4,6 +4,7 @@ import at.asitplus.signum.indispensable.josef.JwsCompact
 import at.asitplus.signum.indispensable.josef.JwsCompactStringSerializer
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import kotlinx.serialization.Transient
 
 /**
  * Part of the ISO/IEC 18013-5:2021 standard: Data structure for Server retrieval mdoc response (8.3.2.2.2.2)
@@ -13,10 +14,13 @@ data class ServerResponse(
     @SerialName("version")
     val version: String,
     @SerialName("documents")
-    val documents: Array<@Serializable(JwsCompactStringSerializer::class) JwsCompact>,
+    val documentsJws: Array<@Serializable(JwsCompactStringSerializer::class) JwsCompact>,
     @SerialName("documentErrors")
     val documentErrors: Map<String, Int>? = null,
 ) {
+    @Transient
+    @Deprecated("Please use documentsJws instead", ReplaceWith("documentsJws"))
+    val documents = documentsJws.map { it.toString() }
 
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
@@ -25,13 +29,13 @@ data class ServerResponse(
         other as ServerResponse
 
         if (version != other.version) return false
-        if (!documents.contentEquals(other.documents)) return false
+        if (!documentsJws.contentEquals(other.documentsJws)) return false
         return documentErrors == other.documentErrors
     }
 
     override fun hashCode(): Int {
         var result = version.hashCode()
-        result = 31 * result + documents.contentHashCode()
+        result = 31 * result + documentsJws.contentHashCode()
         result = 31 * result + (documentErrors?.hashCode() ?: 0)
         return result
     }
