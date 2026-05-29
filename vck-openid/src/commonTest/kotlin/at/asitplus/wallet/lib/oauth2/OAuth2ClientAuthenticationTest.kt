n0900/vck1

OID4VCI: enforce TS3 wallet attestation algorithms

vck-openid/src/commonTest/kotlin/at/asitplus/wallet/lib/oauth2/OAuth2ClientAuthenticationTest.kt
@@ -6,7 +6,9 @@ import at.asitplus.openid.RequestParameters
 import at.asitplus.openid.TokenIntrospectionRequest
 import at.asitplus.openid.TokenIntrospectionResponse
 import at.asitplus.openid.TokenResponseParameters
+import at.asitplus.signum.indispensable.io.Base64UrlStrict
 import at.asitplus.signum.indispensable.josef.JsonWebToken
+import at.asitplus.signum.indispensable.josef.io.joseCompliantSerializer
 import at.asitplus.testballoon.withFixtureGenerator
 import at.asitplus.wallet.lib.agent.EphemeralKeyWithSelfSignedCert
 import at.asitplus.wallet.lib.agent.RandomSource
@@ -28,6 +30,10 @@ import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeInstanceOf
 import io.ktor.http.*
+import io.matthewnelson.encoding.core.Decoder.Companion.decodeToByteArray
+import io.matthewnelson.encoding.core.Encoder.Companion.encodeToString
+import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.jsonObject
 
 val OAuth2ClientAuthenticationTest by testSuite {
 
@@ -180,6 +186,26 @@ val OAuth2ClientAuthenticationTest by testSuite {
             }
         }
 
+        test("pushed authorization request with unsupported client attestation algorithm") {
+            val state = uuid4().toString()
+            val authnRequest = it.client.createAuthRequestJar(
+                state = state,
+                scope = it.scope,
+            )
+
+            shouldThrow<OAuth2Exception> {
+                it.server.par(
+                    authnRequest,
+                    RequestInfo(
+                        url = "https://example.com/",
+                        method = HttpMethod.Post,
+                        clientAttestation = it.clientAttestation.serialize().withHeaderAlg("RS256"),
+                        clientAttestationPop = it.clientAttestationPop.serialize()
+                    )
+                ).getOrThrow()
+            }
+        }
+
         test("pushed authorization request without client authentication") {
             val state = uuid4().toString()
             val authnRequest = it.client.createAuthRequestJar(
@@ -245,4 +271,16 @@ val OAuth2ClientAuthenticationTest by testSuite {
             }
         }
     }
+}
+
+private fun String.withHeaderAlg(alg: String): String {
+    val parts = split(".")
+    require(parts.size == 3) { "Expected compact JWS" }
+    val header = joseCompliantSerializer.parseToJsonElement(
+        parts[0].decodeToByteArray(Base64UrlStrict).decodeToString()
+    ).jsonObject
+    val encodedHeader = joseCompliantSerializer.encodeToString(
+        header.toMutableMap().also { it["alg"] = JsonPrimitive(alg) }
+    ).encodeToByteArray().encodeToString(Base64UrlStrict)
+    return listOf(encodedHeader, parts[1], parts[2]).joinToString(".")
 }

vck-openid/src/commonTest/kotlin/at/asitplus/wallet/lib/oidvci/OidvciAttestationTest.kt
@@ -20,6 +20,7 @@ import at.asitplus.openid.KeyAttestationRequired
 import at.asitplus.openid.OpenIdConstants
 import at.asitplus.openid.RequestParameters
 import at.asitplus.openid.TokenResponseParameters
+import at.asitplus.signum.indispensable.io.Base64UrlStrict
 import at.asitplus.signum.indispensable.josef.JsonWebToken
 import at.asitplus.signum.indispensable.josef.JwsSigned
 import at.asitplus.signum.indispensable.josef.KeyAttestationJwt
@@ -52,8 +53,12 @@ import io.kotest.matchers.collections.shouldNotBeEmpty
 import io.kotest.matchers.nulls.shouldNotBeNull
 import io.kotest.matchers.shouldBe
 import io.kotest.matchers.types.shouldBeInstanceOf
+import io.matthewnelson.encoding.core.Decoder.Companion.decodeToByteArray
+import io.matthewnelson.encoding.core.Encoder.Companion.encodeToString
 import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonPrimitive
 import kotlinx.serialization.json.buildJsonObject
+import kotlinx.serialization.json.jsonObject
 import kotlinx.serialization.json.put
 import kotlinx.serialization.json.putJsonObject
 import kotlin.time.Duration.Companion.days
@@ -358,6 +363,45 @@ val OidvciAttestationTest by testSuite {
             }
         }
 
+        test("reject jwt proof with unsupported algorithm") {
+            it.issuer = CredentialIssuer(
+                authorizationService = it.authorizationService,
+                issuer = IssuerAgent(
+                    identifier = "https://issuer.example.com".toUri(),
+                    randomSource = RandomSource.Default
+                ),
+                credentialSchemes = setOf(ConstantIndex.AtomicAttribute2023, MobileDrivingLicenceScheme),
+                proofValidator = ProofValidator(requireKeyAttestation = false)
+            )
+
+            val requestOptions = RequestOptions(ConstantIndex.AtomicAttribute2023, PLAIN_JWT)
+            val credentialFormat = it.client.selectSupportedCredentialFormat(requestOptions, it.issuer.metadata)
+                .shouldNotBeNull()
+            val scope = credentialFormat.scope.shouldNotBeNull()
+            val token = it.getToken(scope)
+            val clientNonce = it.issuer.nonceWithDpopNonce().getOrThrow().response.clientNonce
+
+            val request = it.client.createCredential(
+                tokenResponse = token,
+                metadata = it.issuer.metadata,
+                credentialFormat = credentialFormat,
+                clientNonce = clientNonce
+            ).getOrThrow().single().shouldBeInstanceOf<WalletService.CredentialRequest.Plain>()
+
+            val tamperedProof = request.request.proofs.shouldNotBeNull().jwt.shouldNotBeNull().single().withHeaderAlg("RS256")
+            val tamperedRequest = request.request.copy(
+                proofs = request.request.proofs!!.copy(jwt = setOf(tamperedProof))
+            )
+
+            shouldThrow<OAuth2Exception> {
+                it.issuer.credential(
+                    authorizationHeader = token.toHttpHeaderValue(),
+                    params = WalletService.CredentialRequest.Plain(tamperedRequest),
+                    credentialDataProvider = DummyOAuth2IssuerCredentialDataProvider,
+                ).getOrThrow()
+            }
+        }
+
     }
 }
 
@@ -391,3 +435,15 @@ private suspend fun WalletService.loadTestKeyAttestation(
 }
 
 private fun dummyUser(): OidcUserInfoExtended = OidcUserInfoExtended.deserialize("{\"sub\": \"foo\"}").getOrThrow()
+
+private fun String.withHeaderAlg(alg: String): String {
+    val parts = split(".")
+    require(parts.size == 3) { "Expected compact JWS" }
+    val header = joseCompliantSerializer.parseToJsonElement(
+        parts[0].decodeToByteArray(Base64UrlStrict).decodeToString()
+    ).jsonObject
+    val encodedHeader = joseCompliantSerializer.encodeToString(
+        header.toMutableMap().also { it["alg"] = JsonPrimitive(alg) }
+    ).encodeToByteArray().encodeToString(Base64UrlStrict)
+    return listOf(encodedHeader, parts[1], parts[2]).joinToString(".")
+}

a-sit-plus/vck

vck-openid/src/commonTest/kotlin/at/asitplus/wallet/lib/oauth2/OAuth2ClientAuthenticationTest.kt
@@ -7,6 +7,8 @@ import at.asitplus.openid.TokenIntrospectionRequest
 import at.asitplus.openid.TokenIntrospectionResponse
 import at.asitplus.openid.TokenResponseParameters
 import at.asitplus.signum.indispensable.josef.JsonWebToken
+import at.asitplus.signum.indispensable.josef.JwsAlgorithm
+import at.asitplus.signum.indispensable.josef.JwsCompactTyped
 import at.asitplus.testballoon.withFixtureGenerator
 import at.asitplus.wallet.lib.agent.EphemeralKeyWithSelfSignedCert
 import at.asitplus.wallet.lib.agent.RandomSource
@@ -180,6 +182,26 @@ val OAuth2ClientAuthenticationTest by testSuite {
             }
         }
 
+        test("pushed authorization request with unsupported client attestation algorithm") {
+            val state = uuid4().toString()
+            val authnRequest = it.client.createAuthRequestJar(
+                state = state,
+                scope = it.scope,
+            )
+
+            shouldThrow<OAuth2Exception> {
+                it.server.par(
+                    authnRequest,
+                    RequestInfo(
+                        url = "https://example.com/",
+                        method = HttpMethod.Post,
+                        clientAttestation = it.clientAttestation.withHeaderAlg(JwsAlgorithm.Signature.RS256),
+                        clientAttestationPop = it.clientAttestationPop
+                    )
+                ).getOrThrow()
+            }
+        }
+
         test("pushed authorization request without client authentication") {
             val state = uuid4().toString()
             val authnRequest = it.client.createAuthRequestJar(
@@ -246,3 +268,8 @@ val OAuth2ClientAuthenticationTest by testSuite {
         }
     }
 }
+
+private suspend fun JwsCompactTyped<JsonWebToken>.withHeaderAlg(alg: JwsAlgorithm.Signature) =
+    JwsCompactTyped<JsonWebToken>(jws.jwsHeader.copy(algorithm = alg), jws.getPayload<JsonWebToken>().getOrThrow()) {
+        jws.signature.rawByteArray
+    }

vck-openid/src/commonTest/kotlin/at/asitplus/wallet/lib/oidvci/OidvciAttestationTest.kt
@@ -15,11 +15,13 @@ package at.asitplus.wallet.lib.oidvci
 import at.asitplus.KmmResult.Companion.wrap
 import at.asitplus.catching
 import at.asitplus.catchingUnwrapped
-import at.asitplus.openid.OidcUserInfoExtended
 import at.asitplus.openid.KeyAttestationRequired
+import at.asitplus.openid.OidcUserInfoExtended
 import at.asitplus.openid.OpenIdConstants
 import at.asitplus.openid.RequestParameters
 import at.asitplus.openid.TokenResponseParameters
+import at.asitplus.signum.indispensable.josef.JwsAlgorithm
+import at.asitplus.signum.indispensable.josef.JwsCompact
 import at.asitplus.signum.indispensable.josef.JwsCompactTyped
 import at.asitplus.signum.indispensable.josef.KeyAttestationJwt
 import at.asitplus.signum.indispensable.josef.KeyStorageStatus
@@ -54,8 +56,8 @@ import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.buildJsonObject
 import kotlinx.serialization.json.put
 import kotlinx.serialization.json.putJsonObject
-import kotlin.time.Duration.Companion.days
 import kotlin.time.Clock.System
+import kotlin.time.Duration.Companion.days
 
 val OidvciAttestationTest by testSuite {
     withFixtureGenerator {
@@ -345,6 +347,47 @@ val OidvciAttestationTest by testSuite {
                 }
             }
         }
+
+        test("reject jwt proof with unsupported algorithm") {
+            it.issuer = CredentialIssuer(
+                authorizationService = it.authorizationService,
+                issuer = IssuerAgent(
+                    identifier = "https://issuer.example.com".toUri(),
+                    randomSource = RandomSource.Default
+                ),
+                credentialSchemes = setOf(ConstantIndex.AtomicAttribute2023, MobileDrivingLicenceScheme),
+                proofValidator = ProofValidator(requireKeyAttestation = false)
+            )
+
+            val requestOptions = RequestOptions(ConstantIndex.AtomicAttribute2023, PLAIN_JWT)
+            val credentialFormat = it.client.selectSupportedCredentialFormat(requestOptions, it.issuer.metadata)
+                .shouldNotBeNull()
+            val scope = credentialFormat.scope.shouldNotBeNull()
+            val token = it.getToken(scope)
+            val clientNonce = it.issuer.nonceWithDpopNonce().getOrThrow().response.clientNonce
+
+            val request = it.client.createCredential(
+                tokenResponse = token,
+                metadata = it.issuer.metadata,
+                credentialFormat = credentialFormat,
+                clientNonce = clientNonce
+            ).getOrThrow().single().shouldBeInstanceOf<WalletService.CredentialRequest.Plain>()
+
+            val tamperedProof = request.request.proofs.shouldNotBeNull().jwt.shouldNotBeNull().single()
+                .withHeaderAlg(JwsAlgorithm.Signature.RS256)
+            val tamperedRequest = request.request.copy(
+                proofs = request.request.proofs!!.copy(jwt = setOf(tamperedProof))
+            )
+
+            shouldThrow<OAuth2Exception> {
+                it.issuer.credential(
+                    authorizationHeader = token.toHttpHeaderValue(),
+                    params = WalletService.CredentialRequest.Plain(tamperedRequest),
+                    credentialDataProvider = DummyOAuth2IssuerCredentialDataProvider,
+                ).getOrThrow()
+            }
+        }
+
     }
 }
 
@@ -378,3 +421,8 @@ private suspend fun WalletService.loadTestKeyAttestation(
 }
 
 private fun dummyUser(): OidcUserInfoExtended = OidcUserInfoExtended.deserialize("{\"sub\": \"foo\"}").getOrThrow()
+
+private suspend fun JwsCompact.withHeaderAlg(alg: JwsAlgorithm.Signature): JwsCompact =
+    JwsCompact(jwsHeader.copy(algorithm = alg), plainPayload) {
+        signature.rawByteArray
+    }