0ae682cc4b55315a6d12419fbba5f6615e722504
Bernd Prünster <bernd.pruenster@a-sit.at>
2026-06-10 16:20:56 +0200

    migrate new tests

--- openid-data-classes/src/commonTest/kotlin/at/asitplus/openid/SupportedCredentialFormatTest.kt
+++ openid-data-classes/src/commonTest/kotlin/at/asitplus/openid/SupportedCredentialFormatTest.kt
@@ -1,21 +1,21 @@
 package at.asitplus.openid
 
-import at.asitplus.testballoon.withData
-import at.asitplus.testballoon.withDataSuites
-import de.infix.testBalloon.framework.core.testSuite
+import at.asitplus.testballoon.matrix.matrixSuite
 import io.kotest.matchers.types.shouldBeInstanceOf
 import kotlinx.serialization.json.Json
 
 @Suppress("unused")
-val SupportedCredentialFormatTest by testSuite {
+val SupportedCredentialFormatTest by matrixSuite {
     testSuite("mdoc format deserialization") {
-        withDataSuites(
-            """{"doctype": "eu.pid.1", "format": "mso_mdoc"}""",
-        ) { serialized ->
-            withData(
-                SupportedCredentialFormat.serializer(),
-                SupportedCredentialFormatIsoMdoc.serializer(),
-            ) { serializer ->
+        data(
+            listOf("""{"doctype": "eu.pid.1", "format": "mso_mdoc"}"""),
+        ) - { serialized ->
+            data(
+                listOf(
+                    SupportedCredentialFormat.serializer(),
+                    SupportedCredentialFormatIsoMdoc.serializer(),
+                )
+            ) test { serializer ->
                 Json.decodeFromString(
                     serializer,
                     serialized
@@ -25,13 +25,17 @@ val SupportedCredentialFormatTest by testSuite {
     }
 
     testSuite("sd jwt format deserialization") {
-        withDataSuites(
-            """{"vct": "eu.pid.1", "format": "dc+sd-jwt"}""",
-        ) { serialized ->
-            withData(
-                SupportedCredentialFormat.serializer(),
-                SupportedCredentialFormatSdJwt.serializer(),
-            ) { serializer ->
+        data(
+            listOf(
+                """{"vct": "eu.pid.1", "format": "dc+sd-jwt"}""",
+            )
+        ) - { serialized ->
+            data(
+                listOf(
+                    SupportedCredentialFormat.serializer(),
+                    SupportedCredentialFormatSdJwt.serializer(),
+                )
+            ) test { serializer ->
                 Json.decodeFromString(
                     serializer,
                     serialized
@@ -41,13 +45,17 @@ val SupportedCredentialFormatTest by testSuite {
     }
 
     testSuite("w3c vc jwt format deserialization") {
-        withDataSuites(
-            """{ "credential_definition": { "type": ["eu.pid.1"] }, "format": "jwt_vc_json"}""".trimMargin(),
-        ) { serialized ->
-            withData(
-                SupportedCredentialFormat.serializer(),
-                SupportedCredentialFormatW3cVcJwt.serializer(),
-            ) { serializer ->
+        data(
+            listOf(
+                """{ "credential_definition": { "type": ["eu.pid.1"] }, "format": "jwt_vc_json"}""".trimMargin(),
+            )
+        ) - { serialized ->
+            data(
+                listOf(
+                    SupportedCredentialFormat.serializer(),
+                    SupportedCredentialFormatW3cVcJwt.serializer(),
+                )
+            ) test { serializer ->
                 Json.decodeFromString(
                     serializer,
                     serialized
@@ -57,13 +65,15 @@ val SupportedCredentialFormatTest by testSuite {
     }
 
     testSuite("w3c vc json ld format deserialization") {
-        withDataSuites(
-            """{ "credential_definition": { "@context": ["${W3cVerifiableCredentialsContext.FIRST}"], "type": ["eu.pid.1"] }, "format": "ldp_vc"}""",
-        ) { serialized ->
-            withData(
-                SupportedCredentialFormat.serializer(),
-                SupportedCredentialFormatW3cVcJsonLd.serializer(),
-            ) { serializer ->
+        data(
+            listOf("""{ "credential_definition": { "@context": ["${W3cVerifiableCredentialsContext.FIRST}"], "type": ["eu.pid.1"] }, "format": "ldp_vc"}"""),
+        ) - { serialized ->
+            data(
+                listOf(
+                    SupportedCredentialFormat.serializer(),
+                    SupportedCredentialFormatW3cVcJsonLd.serializer(),
+                )
+            ) test { serializer ->
                 Json.decodeFromString(
                     serializer,
                     serialized
@@ -73,13 +83,17 @@ val SupportedCredentialFormatTest by testSuite {
     }
 
     testSuite("w3c vc jwt + json ld format deserialization") {
-        withDataSuites(
-            """{ "credential_definition": { "@context": ["${W3cVerifiableCredentialsContext.FIRST}"], "type": ["eu.pid.1"] }, "format": "jwt_vc_json-ld"}""",
-        ) { serialized ->
-            withData(
-                SupportedCredentialFormat.serializer(),
-                SupportedCredentialFormatW3cVcJwtJsonLd.serializer(),
-            ) { serializer ->
+        data(
+            listOf(
+                """{ "credential_definition": { "@context": ["${W3cVerifiableCredentialsContext.FIRST}"], "type": ["eu.pid.1"] }, "format": "jwt_vc_json-ld"}""",
+            )
+        ) - { serialized ->
+            data(
+                listOf(
+                    SupportedCredentialFormat.serializer(),
+                    SupportedCredentialFormatW3cVcJwtJsonLd.serializer(),
+                )
+            ) test { serializer ->
                 Json.decodeFromString(
                     serializer,
                     serialized
