921451256ea7fee3455657c586b916d98eccad56
Henrik Zant <henrik.zant@tugraz.at>
2026-09-23 00:36:34 +0200

    Add developer info

--- vck-longfellow/build.gradle.kts
+++ vck-longfellow/build.gradle.kts
@@ -70,6 +70,11 @@ publishing {
                         name.set("Christian Kollmann")
                         email.set("christian.kollmann@a-sit.at")
                     }
+                    developer {
+                        id.set("opsiv")
+                        name.set("Henrik Zant")
+                        email.set("henrik.zant@a-sit.at")
+                    }
                 }
                 scm {
                     connection.set("scm:git:git@github.com:a-sit-plus/vck.git")
